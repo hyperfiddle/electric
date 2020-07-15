6586d97cd95f8c192875c1a847d210f8c0e9802f
Gustavs T <g@compose.lv>
2020-07-15 14:43:17 +0100

    Entry point

---
+++ src/hyperfiddle/hx/test/DFTest.hx
@@ -0,0 +1,33 @@
+package hyperfiddle.hx.test;
+import hyperfiddle.hx.DF;
+
+class DFTest {
+  static function main() {
+    var a = Origin.input();
+    var b = Origin.input();
+
+    var am = Origin.apply([a], x -> '$x!');
+    var ab = Origin.apply([am, b], (a, b) -> [a,b]);
+
+    Origin.on(am, x -> trace("map:", x));
+    Origin.on(ab, x -> trace("join:", x));
+
+    var x1 = Origin.apply([a, am], (a, b) -> [a,b]);
+    var x2 = Origin.apply([a, ab, b], (a, b, c) -> [a,b,c]);
+
+    Origin.on(x1, x -> trace("join2:", x));
+    Origin.on(x2, x -> trace("join3:", x));
+
+    a.put("a");
+    b.put("b");
+    a.put("a2");
+    b.put("b2");
+    b.end();
+    a.put("a3");
+
+    Origin.all(() -> {
+      a.put("az");
+      b.put("bz");
+    });
+  }
+}
