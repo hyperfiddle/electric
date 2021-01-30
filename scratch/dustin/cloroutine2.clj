21299686fb4ca9b08f9e431ed7726f1acf1024d1
Dustin Getz <dustin.getz@gmail.com>
2021-01-30 14:57:38 -0500

    cloroutine2

---
+++ scratch/dustin/cloroutine2.clj
@@ -0,0 +1,40 @@
+(ns dustin.cloroutine2
+  (:require
+    [cloroutine.core :refer [cr]]
+    [cloroutine.impl :as i]
+    [minitest :refer [tests]])
+  (:import [java.util.concurrent CompletableFuture]))
+
+(tests
+  "java stuff prerequisites"
+  (def x (reify java.util.function.BiConsumer
+           (accept [this v e]
+             (println 'accept v e)
+             ::done)))
+  (.accept x 1 2) := nil                                   ; for effect
+  )
+
+(def ^:dynamic *fiber*)
+(def ^:dynamic *value*)
+(def ^:dynamic *error*)
+
+(defn await [cf] (.whenComplete cf *fiber*))
+(defn thunk [] (println 'thunk *value*) *value*)
+
+(defmacro async [& body]
+  `(let [cf# (CompletableFuture.)
+         cr# (cr {await thunk}
+               (.complete cf# (do ~@body)))]
+     (binding [*fiber* (reify java.util.function.BiConsumer
+                         (accept [this# v# e#]
+                           (binding [*fiber* this#
+                                     *value* v#]
+                             (println 'accept)
+                             (cr#))))]
+       (cr#))
+     cf#))
+
+(tests
+  (def six (async 6))
+  (def seven (async (inc (await six))))
+  )
