9812e8196967b3fe44935ab6f904683ca4f26842
Léo NOEL <contact@leonoel.fr>
2021-02-05 17:43:30 +0100

    dataflow normalizer, locals + if

--- scratch/leo/dataflow.clj
+++ scratch/leo/dataflow.clj
@@ -1,6 +1,7 @@
 (ns leo.dataflow
   (:refer-clojure :exclude [compile])
   (:require [clojure.tools.analyzer.jvm :as clj]
+            [cljs.analyzer.api :as cljs]
             [minitest :refer [tests]])
   (:import (clojure.lang Compiler$LocalBinding)))
 
@@ -20,37 +21,51 @@
                               :name symbol}))
                        binding))) {})]
     (fn [env form]
-      (binding [clj/run-passes clj/scheduled-default-passes]
-        (->> env
-          (scope-bindings)
-          (update (clj/empty-env) :locals merge)
-          (clj/analyze form))))))
+      (if (:js-globals env)
+        (cljs/analyze env form)
+        (binding [clj/run-passes clj/scheduled-default-passes]
+          (->> env
+            (scope-bindings)
+            (update (clj/empty-env) :locals merge)
+            (clj/analyze form)))))))
 
-(def analyze-ast
-  (letfn [(sexp [stack ast]
-            (let [graph (walk (peek stack) ast)]
-              (conj (pop stack) (-> graph count dec) graph)))
-          (walk [graph {:keys [op] :as ast}]
+(def normalize-ast
+  (letfn [(walk [env {:keys [op] :as ast}]
             (case op
-              (:const :var)
-              (conj graph {:input (case op :const (:val ast) :var (:var ast))})
+              (:const)
+              [:input (:val ast)]
+
+              (:var)
+              [:input (symbol (:var ast))]
 
               (:static-call)
-              (let [stack (reduce sexp [graph] (:args ast))]
-                (conj (peek stack)
-                  {:type :derived
-                   :deps (pop stack)
-                   :proc (str (.getName ^Class (:class ast))
-                           "/" (name (:method ast)))}))
+              (into [:static (symbol (.getName ^Class (:class ast))
+                               (name (:method ast)))]
+                (map (partial walk env)) (:args ast))
 
               (:invoke)
-              (let [stack (reduce sexp [graph] (cons (:fn ast) (:args ast)))]
-                (conj (peek stack)
-                  {:type :derived
-                   :deps (pop stack)}))
+              (into [:apply] (map (partial walk env))
+                (cons (:fn ast) (:args ast)))
+
+              (:do)
+              (walk env (:ret ast))                         ;; fantasyland, discard statements
+
+              (:let)
+              (walk (reduce (fn [env {:keys [name init]}]
+                              (assoc env name (walk env init)))
+                      env (:bindings ast)) (:body ast))
+
+              (:local)
+              (or (env (:name ast)) [:input (:name ast)])
+
+              (:if)
+              [:if
+               (walk env (:test ast))
+               (walk env (:then ast))
+               (walk env (:else ast))]
 
               ))]
-    (partial walk [])))
+    (partial walk {})))
 
 (defn emit [prefix graph]
   graph)
@@ -58,7 +73,7 @@
 (defn compile [main env prefix]
   (->> main
     (analyze-clj env)
-    (analyze-ast)
+    (normalize-ast)
     (emit prefix)))
 
 (defmacro dataflow [main]
@@ -66,6 +81,33 @@
 
 (tests
   (macroexpand '(dataflow (+ 1 2))) :=
-  [{:input 1} {:input 2} {:type :derived, :proc "clojure.lang.Numbers/add", :deps [0 1]}]
+  [:static 'clojure.lang.Numbers/add [:input 1] [:input 2]]
+
+  (macroexpand '(dataflow (let [a 1] (+ a a)))) :=
+  [:static 'clojure.lang.Numbers/add [:input 1] [:input 1]]
+
+  (def !input (atom 0))
+  (require '[missionary.core :as m])
+  (macroexpand '(dataflow (let [i (m/watch !input)]
+                            (if (odd? i) (inc i) i)))) :=
+  '[:if
+    [:apply [:input clojure.core/odd?] [:apply [:input missionary.core/watch] [:input leo.dataflow/!input]]]
+    [:static clojure.lang.Numbers/inc [:apply [:input missionary.core/watch] [:input leo.dataflow/!input]]]
+    [:apply [:input missionary.core/watch] [:input leo.dataflow/!input]]]
+
+  )
+
+(comment
+  (def !input (atom 0))
+  (def !input2 (atom 0))
+
+  (deflow f [x]
+    (if (even? (<- x))
+      (inc (<- x)) 0))
+
+  (dataflow
+    (let [i (<- (m/watch !input))]
+      [(if (odd? i) (inc i) ((fn [] i)))
+       (* (f (m/watch !input2)) 2)]))
 
   )
