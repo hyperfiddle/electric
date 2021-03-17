0860f396da3c447eb1465aa5398e4f29424eeaab
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2021-03-17 16:36:30 +0100

    Merged tests, cleaned results

--- scratch/dustin/lang_leo.clj
+++ scratch/dustin/lang_leo.clj
@@ -1,17 +1,46 @@
-(ns dustin.dataflow7
+(ns dustin.lang-leo
   "tests to communicate about leo lang"
+
+  ; What is our mission ?
+  ; Produce test cases that bring the right questions
+  ; that make us enquire about design choices
+
+  ; Dustin: Ask questions at .9999999 precision
+  ; Dustin: A question at .9 precision does not qualify as a valid question
+
+  ; How will we do it? What artifact will we produce?
+  ; create one file lang-leo with 200 LOC test cases
+  ; we will test from hello-world to let, if, case, destructuring
+  ; what operations introduce signals? We don't know
+  ; effects?
+  ; reactive-for
+  ; Out of scope: ifn, $, nested dags (wait for .99999 precision before moving on to this)
+
+  ; What will D/G do tomorrow? We will read the cases and Dustin will agree that this matches
+  ; our understanding of leo-lang and then pass on to Leo
+
+  ; What are we optimizing for?
+  ; Not interrupting Leo because he's the severe bottleneck
+  ; Build a foundation to communicate on top of
+
+
+
+  ; OODA
+  ; observe -> orient -> decide -> act ->
+
+
   (:require [hfdl :refer [dataflow]]
             [minitest :refer [tests]]
             [missionary.core :as m]))
 
-(test
+(tests
 
   "programs without variability terminate immediately"
   (def program (dataflow 1))
   (def process (debug! program))                            ; (trace!) ?
-  @process := {:state       :terminated                     ; Because the program is entirely static
-               :result      1                               ; ??? it terminated with 1
-               :signal-heap {}}                             ; GG: Process is terminated -> heap was GCd.
+  @process := {:state  :terminated                          ; Because the program is entirely static
+               :result 1                                    ; ??? it terminated with 1
+               :vars   {}}
 
   ; how does shell user ask the entrypoint for the result of a program that maybe terminated?
   ; need a "shell" - C-z, C-c, print result eventually
@@ -20,9 +49,9 @@
   (def a 1)                                                 ; value
   (def program (dataflow a))                                ; clojure interop with a value
   (def process (debug! program))
-  @process := {:state       :terminated
-               :result      1
-               :signal-heap {}}
+  @process := {:state  :terminated
+               :result 1
+               :vars   {}}
 
   "clojure foreign interop to a variable"
   ; variable is not overloaded in clojure so we can take that word
@@ -30,41 +59,59 @@
   (def a (m/watch !a))                                      ; HFDL variable (missionary flow)
   (def program (dataflow @a))                               ; clojure interop with a HFDL variable
   (def process (debug! program))
-  @process := {:state       :running                        ; maintain result forever
-               :result      1                               ; ??? point of dataflow is to cause effects
-               :signal-heap {'a 1}}                         ; ??? should inbound signals show up in the heap?
+  @process := {:state :running                              ; maintain result forever
+               :vars  {'a 1}}                               ; ??? should inbound signals show up in the heap?
                                                             ; GG: thinks yes, the deref call on `a` shows up in the heap.
 
   "maintain result in response to variable changes"
   (swap ! !a inc)
+  @process := {:state :running
+               :vars  {'a 2}}
+
+  "Transition between running and terminated state"
+  (def !a (atom 0))
+  (def a (->> (m/watch !a)
+              (m/transform (take-while even?))))
+  ​
+  (def program (dataflow @a))
+  (def process (debug! program))
+  ​
   @process := {:state       :running
-               :result      2
-               :signal-heap {'a 2}}
+               :signal-heap {'a 0}}
+  ​
+  (reset! !a 2)
+  ​
+  @process := {:state       :running
+               :signal-heap {'a 0}}
+  ​
+  (reset! !a 1)
+  ​
+  @process := {:state       :terminated
+               :result      1
+               :signal-heap {}}
 
   "clojure foreign fn call"
   (def f inc)
   (def program (dataflow (f 1)))
   (def process (debug! program))
-  @process := {:state       :terminated
-               :result      2
-               :signal-heap {}}
+  @process := {:state  :terminated
+               :result 2
+               :vars   {}}
 
   "composition with variables, no signals"
   (def !a (atom 1)) (def a (m/watch !a))
   (def !b (atom 2)) (def b (m/watch !b))
   (def program (dataflow (+ @a (inc @b))))                  ; ??? can you auto add signals? What are the rules
   (def process (debug! program))
-  @process := {:state       :running
-               :result      3
-               :signal-heap {'a 1                           ; the watches are in the heap
-                             'b 2}}                         ; but no incremental maintenance. Slow!
+  @process := {:state  :running
+               :vars   {'a 1                                ; the watches are in the heap
+                        'b 2}}                              ; but no incremental maintenance. Slow!
   ; TODO draw viz, it has one node
   ; it fused, like this: ((fn [a b] (+ a (inc b))) @a @b)
 
   (swap! !b inc)
-  @process := {:state       :running
-               :result      4
-               :signal-heap {'a 1 'b 2}}
+  @process := {:state  :running
+               :vars   {'a 1 'b 2}}
 
   "composition with variables, with signals"
   (def !a (atom 1)) (def a (m/watch !a))
@@ -73,20 +120,19 @@
                                b2 @b]
                            (+ a2 (inc b2)))))
   (def process (debug! program))
-  @process := {:state       :running
-               :result      3
-               :signal-heap {'a2 1 'b2 2} #_{'a 1 'b 2}}    ; ??? Does let introduces signals? Deref?
+  @process := {:state :running
+               :vars  {'a2 1 'b2 2} #_{'a 1 'b 2}}          ; ??? Does let introduces signals? Deref?
+
   (swap! !b inc)
-  @process := {:state       :running
-               :result      4
-               :signal-heap {'a2 1 'b2 3} #_{'a 1 'b 2}}
+  @process := {:state :running
+               :vars  {'a2 1 'b2 3} #_{'a 1 'b 2}}
 
   "composition with no variables"
   (def program (dataflow (+ 1 (inc 2))))                    ; still static
   (def process (debug! program))
-  @process := {:state       :terminated
-               :result      3
-               :signal-heap {}}
+  @process := {:state  :terminated
+               :result 3
+               :vars   {}}
   ; ??? Draw the viz, there are no signals in this static program
 
   "clojure foreign fn call in response to variability"
@@ -95,52 +141,43 @@
   (def f inc)
   (def program (dataflow (f @a)))
   (def process (debug! program))
-  @process := {:state       :running
-               :result      2
-               :signal-heap {'a 1}}
+  @process := {:state  :running
+               #_#_:result 2                                ; ??? Result would be different then 'a, do we want to track it?
+               :vars   {'a 1}}
 
   "incrementally maintain result in response to variable change"
   (swap ! !a inc)
-  @process := {:state       :running
-               :result      3
-               :signal-heap {'a 2}}
-
-  "combined example - incremental maintain clojure computation with variable"
-  (def !a (atom 1))
-  (def a (m/watch !a))
-  (def program (m/cp (println (dataflow (+ @a 2)))))        ; ??? How to bridge RT dataflow exprs to OS effects
-  (def process (debug! program))
-  @process := {:state       :running
-               :result      3
-               :signal-heap {'a 1}}
+  @process := {:state  :running
+               #_#_:result 3                                ; ??? Result would be different then 'a, do we want to track it?
+               :vars   {'a 2}}
 
   "dataflow apply (reactive fn)"
   (def !f (atom +)) (def f (m/watch !f))
   (def !a (atom 1)) (def a (m/watch !a))
   (def program (dataflow (@f @a 2)))                        ; variable function in fncall position
   (def process (debug! program))
-  @process := {:state       :running
-               :result      3
-               :signal-heap {'f +
-                             'a 1}}
+  @process := {:state  :running
+               #_#_:result 3
+               :vars   {'f +
+                        'a 1}}
   (reset! !a 11)
-  @process := {:state       :running
-               :result      13
-               :signal-heap {'f +
-                             'a 11}}
+  @process := {:state  :running
+               #_#_:result 13
+               :vars   {'f +
+                        'a 11}}
   (reset! !f -)
-  @process := {:state       :running
-               :result      9
-               :signal-heap {'f -
-                             'a 11}}
+  @process := {:state  :running
+               #_#_:result 9
+               :vars   {'f -
+                        'a 11}}
 
   "simplest diamond, no let"
   (def !a (atom 1)) (def a (m/watch !a))
   (def program (dataflow (+ @a @a)))                        ; ??? how many times does @ appear in the viz?
   (def process (debug! program))
-  @process := {:state       :running
-               :result      2
-               :signal-heap {'a 1}}
+  @process := {:state  :running
+               #_#_:result 2
+               :vars   {'a 1}}
 
   ; Dustin opinion:
   ; the ast should match the viz
@@ -150,14 +187,22 @@
   "same thing with let?"
   (def !a (atom 1)) (def a (m/watch !a))
   (def program (dataflow
-                 (let [a' @a]
-                   (+ a' a'))))                             ; this is just clojure, 100% foreign call
+                 (let [b @a]
+                   (+ b b))))                             ; this is just clojure, 100% foreign call
   ; feels like reagent? (not a bad thing)
   (def process (debug! program))
+  @process := {:state  :running
+               :vars   {'a 1       ;; ??? Not introduced by let (meaningful?)
+                        #_#_'b 1}} ;; ??? Introduced by let     (meaningful?)
+
+  "combined example - incremental maintain clojure computation with variable"
+  (def !a (atom 1))
+  (def a (m/watch !a))
+  (def program (m/cp (println (dataflow (+ @a 2)))))        ; ??? How to bridge RT dataflow exprs to OS effects
+  (def process (debug! program))
   @process := {:state       :running
-               :result      2
-               :signal-heap {'a  1   ;; ??? Not introduced by let (meaningful?)
-                             'a' 1}} ;; ??? Introduced by let     (meaningful?)
+               :result      3
+               :vars {'a 1}}
 
   ; Same heap, same result, different program, same flowchart viz (?)
   ; Why are there two ways to represent the same dataflow structure?
@@ -172,13 +217,17 @@
 
   ; Dustin/Geoffrey cutoff point, todo go through this
 
-  "what is let with a literal"
-  (def program (dataflow (let [a 1] a)))                    ; introduce signal?
-  @process := {:state       :terminated
-               :result      1
-               :signal-heap {'a 1}}                         ; ??? GG: Should 'a be listed here since it comes from a litteral
+  ;; GG: Nonsense, seems to indicate let don't introduces signals, `@` does.
+  "Let introduces a signal on constrants"
+  (def !b (atom 0)) (def b (m/watch !b))
+  (def program (dataflow (let [a 1]
+                           [a @b])))
+  @process := {:state       :running
+               :signal-heap {'a 1
+                             'b 0}}
 
-  ;"diamond"
+
+  ;"Diamond"
   ;(def !a (atom 0))
   ;(def a (m/watch !a))
   ;
