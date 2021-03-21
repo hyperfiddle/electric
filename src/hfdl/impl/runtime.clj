eb69131cc393c8918214286df1701ef4c2059c80
Léo NOEL <contact@leonoel.fr>
2021-03-21 11:26:41 +0100

    HFDL - dataflow spawn on deref

--- src/hfdl/impl/runtime.clj
+++ src/hfdl/impl/runtime.clj
@@ -1,6 +1,7 @@
 (ns hfdl.impl.runtime
-  (:require [missionary.core :as m])
-  (:import (java.util.concurrent.atomic AtomicReferenceArray AtomicInteger AtomicReference)
+  (:require [missionary.core :as m]
+            [hfdl.impl.compiler :refer [with-ctx]])
+  (:import (java.util.concurrent.atomic AtomicInteger AtomicReference)
            (clojure.lang IFn IDeref IRef Box)))
 
 (defn call
@@ -26,8 +27,13 @@
       (if (.compareAndSet r x t)
         (do) (recur)) (t))))
 
-;; TODO error handling
-(defn join "
+(defn cancel-frame! [ar]
+  (dotimes [i (alength ar)]
+    ((aget ar i))))
+
+"
+legacy join doc, TODO error handling
+
 Turns a continuous flow of continuous flows into a continuous flow running successive flows produced by samplings of
 outer flow and producing samplings of the latest inner flow. Inner flows must be ready to transfer immediately on start.
 Resulting flow becomes ready to transfer when either the outer flow or the current inner flow becomes ready to transfer.
@@ -44,124 +50,139 @@ Implementation :
 2. `token` : `nil` when flow is cancelled, otherwise the function to call on cancellation. The cancellation function is
 the outer flow iterator until it's terminated, the inner flow iterator afterwards.
 3. `alive` : the number of non-terminated iterators (outer + inners).
-" [input]
-  (fn [n t]
-    (let [state (AtomicReference. nop)
-          token (AtomicReference. nop)
-          alive (AtomicInteger. 1)
-          done! #(when (zero? (.decrementAndGet alive)) (t))
-          outer (input
-                  #(if-some [s (.get state)]
-                     (if (.compareAndSet state s nil)
-                       (if (instance? Box s) ((.-val ^Box s)) (do (s) (n))) (recur))
-                     (if (.compareAndSet state nil state) (do) (recur)))
-                  #(do (loop []
-                         (if-some [s (.get state)]
-                           (swap-token! token (if (instance? Box s) (.-val ^Box s) s))
-                           (if (.compareAndSet state nil token)
-                             (do) (recur)))) (done!)))]
-      (.set token outer)
-      (reify
-        IFn
-        (invoke [_]
-          (loop []
-            (when-some [t (.get token)]
-              (if (.compareAndSet token t nil)
-                (t) (recur)))))
-        IDeref
-        (deref [_]
-          @(loop []
-             (if-some [b (.get state)]
-               (let [i (.-val ^Box b)]
-                 (if (.compareAndSet state b i)
-                   i (recur)))
-               (loop []
-                 (.incrementAndGet alive)
-                 (let [b (Box. nil)
-                       i (@outer
-                           #(if-some [i (.-val b)]
-                              (if (.compareAndSet state i b)
-                                (n) (try @i (catch Throwable _)))
-                              (set! (.-val b) b)) done!)]
-                   (when (nil? (.-val b))
-                     (throw (IllegalStateException. "Join failure : inner flow has no initial value.")))
-                   (set! (.-val b) i)
-                   (if (.compareAndSet state nil i)
-                     i (if (.compareAndSet state state nil)
-                         (do (i) (try @i (catch Throwable _)) (recur))
-                         (do (swap-token! token i) (.set state i) i))))))))))))
-
-(defn spawn [x]
-  (throw (UnsupportedOperationException.)))
-
-(defn node! [frame idx [op & args]]
-  (let [flow (case op
-               :spawn
-               (spawn (aget frame (first args)))
-               :apply
-               (apply m/latest call (aget frame (first args)) (map (partial aget frame) (second args)))
-               :local
-               (pure (first args))
-               :global
-               (pure (resolve (first args)))
-               :constant
-               (pure (aget frame (first args)))
-               :variable
-               (join (aget frame (first args))))]
-    (doto frame (aset idx (m/signal! flow)))))
-
-(defn frame! [graph]
-  (reduce-kv node! (object-array (count graph)) graph))
-
-(defn trace! [frame]
-  (m/stream!
-    (m/relieve merge
-      (m/ap
-        (let [i (m/?= (m/enumerate (range (alength frame))))]
-          {i (m/?! (aget frame i))})))))
+"
+(defn boot [on-slot]
+  (fn frame! [path graph]
+    (let [ar (object-array (count graph))]
+      (reduce-kv
+        (fn [_ idx [op & args]]
+          (let [path (conj path idx)]
+            (->> (case op
+                   :apply
+                   (apply m/latest call (aget ar (first args)) (map (partial aget ar) (second args)))
+                   :local
+                   (pure (first args))
+                   :global
+                   (pure (resolve (first args)))
+                   :constant
+                   (pure (aget ar (first args)))
+                   :variable
+                   (fn [n t]
+                     (let [state (AtomicReference. nop)
+                           token (AtomicReference. nop)
+                           alive (AtomicInteger. 1)
+                           done! #(when (zero? (.decrementAndGet alive)) (t))
+                           outer ((aget ar (first args))
+                                   #(if-some [s (.get state)]
+                                      (if (.compareAndSet state s nil)
+                                        (if (instance? Box s) ((.-val ^Box s)) (do (s) (n))) (recur))
+                                      (if (.compareAndSet state nil state) (do) (recur)))
+                                   #(do (loop []
+                                          (if-some [s (.get state)]
+                                            (swap-token! token (if (instance? Box s) (.-val ^Box s) s))
+                                            (if (.compareAndSet state nil token)
+                                              (do) (recur)))) (done!)))
+                           counter (int-array 1)
+                           context (fn [dataflow n t]
+                                     (let [ar (frame!
+                                                (conj path
+                                                  (doto (aget counter 0)
+                                                    (->> unchecked-inc-int
+                                                      (aset counter 0))))
+                                                (:graph dataflow))
+                                           it ((aget ar (:result dataflow)) n t)]
+                                       (reify
+                                         IFn
+                                         (invoke [_] (cancel-frame! ar))
+                                         IDeref
+                                         (deref [_] @it))))]
+                       (.set token outer)
+                       (reify
+                         IFn
+                         (invoke [_]
+                           (loop []
+                             (when-some [t (.get token)]
+                               (if (.compareAndSet token t nil)
+                                 (t) (recur)))))
+                         IDeref
+                         (deref [_]
+                           (with-ctx context
+                             @(loop []
+                                (if-some [b (.get state)]
+                                  (let [i (.-val ^Box b)]
+                                    (if (.compareAndSet state b i)
+                                      i (recur)))
+                                  (loop []
+                                    (.incrementAndGet alive)
+                                    (let [b (Box. nil)
+                                          i (@outer
+                                              #(if-some [i (.-val b)]
+                                                 (if (.compareAndSet state i b)
+                                                   (n) (try @i (catch Throwable _)))
+                                                 (set! (.-val b) b)) done!)]
+                                      (when (nil? (.-val b))
+                                        (throw (IllegalStateException. "Join failure : inner flow has no initial value.")))
+                                      (set! (.-val b) i)
+                                      (if (.compareAndSet state nil i)
+                                        i (if (.compareAndSet state state nil)
+                                            (do (i) (try @i (catch Throwable _)) (recur))
+                                            (do (swap-token! token i) (.set state i) i)))))))))))))
+              (m/signal!)
+              (aset ar idx)
+              (on-slot path))))
+        nil graph) ar)))
 
 (defn foreach [f input]
   (m/ap (m/? (f (m/?? input)))))
 
-(defn process [graph out]
-  (m/reactor (m/stream! (foreach out (trace! (frame! graph))))))
+(defn slot-changes [slots]
+  (m/ap
+    (let [[path slot] (m/?= (m/enumerate (m/?= slots)))]
+      {path (m/?? slot)})))
 
-(def merge-vec (partial reduce-kv assoc))
-
-(defn debug! [{:keys [graph]}]
-  (let [vols (doto (AtomicReferenceArray. 2)
-               (.set 0 {:status :running
-                        :slots (vec (repeat (count graph) ::uninitialized))})
-               (.set 1 {}))
-        vars (doto (object-array 1))
+(defn debug! [dataflow]
+  (let [state (AtomicReference. {:status :running :log []})
+        watches (AtomicReference. {})
+        reactor (Box. nil)
+        on-slot (Box. nil)
         public (reify
                  IFn
                  (invoke [_]
-                   ((aget vars 0)))
+                   ((.-val reactor)))
                  IDeref
                  (deref [_]
-                   (.get vols 0))
+                   (.get state))
                  IRef
                  (getWatches [_]
-                   (.get vols 1))
+                   (.get watches))
                  (addWatch [this k cb]
                    (loop []
-                     (let [w (.get vols 1)]
-                       (if (.compareAndSet vols 1 w (assoc w k cb))
+                     (let [w (.get watches)]
+                       (if (.compareAndSet watches w (assoc w k cb))
                          this (recur)))))
                  (removeWatch [this k]
                    (loop []
-                     (let [w (.get vols 1)]
-                       (if (.compareAndSet vols 1 w (dissoc w k))
+                     (let [w (.get watches)]
+                       (if (.compareAndSet watches w (dissoc w k))
                          this (recur))))))
         event! (fn [f & args]
-                 (let [x (.get vols 0)
+                 (let [x (.get state)
                        y (apply f x args)]
-                   (.set vols 0 y)
+                   (.set state y)
                    (reduce-kv (fn [_ k cb] (cb k public x y))
-                     nil (.get vols 1))))]
-    (aset vars 0
-      ((process graph #(m/sp (event! update :slots merge-vec %)))
+                     nil (.get watches))))]
+    (set! (.-val reactor)
+      ((m/reactor
+         (let [slot-feed (->> (m/observe (fn [!] (set! (.-val on-slot) (comp ! hash-map)) nop))
+                           (m/relieve merge)
+                           (m/stream!))]
+           ((boot (.-val on-slot)) [] (:graph dataflow))
+           (->> slot-feed
+             (slot-changes)
+             (m/relieve merge)
+             (m/stream!)
+             (foreach #(m/sp (event! update :log conj %)))
+             (m/stream!))))
        (fn [_] (event! assoc :status :terminated))
        (fn [e] (event! assoc :status :crashed :error e))))
     public))
