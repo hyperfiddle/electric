8d62f41d7e637074039bc0c40a37d20a3c86036f
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2021-04-23 09:05:17 +0200

    [partial] HFQL 26

--- scratch/dustin/hfql/hfql26.clj
+++ scratch/dustin/hfql/hfql26.clj
@@ -2,10 +2,12 @@
 ;;
 ;; * HFQL transfer at every layer
 ;;
-;;   Same as HFQL25 but add hf/render
+;;   Same as HFQL25 but add hf/render (continue)
 ;;
-;; ** STARTED add hf/render defaulting to transfer identity
+;; ** DONE add hf/render defaulting to transfer identity
+;;    CLOSED: [2021-04-23 ven. 09:04]
 ;;
+;;    - State "DONE"       from "STARTED"    [2021-04-23 ven. 09:04]
 ;;    - State "STARTED"    from "TODO"       [2021-04-22 jeu. 15:33]
 ;;    #+begin_src clojure :eval never
 ;;    @(reactive-for (fn [%]
@@ -19,9 +21,13 @@
 ;;
 ;;
 ;;    Up next:
-;;    - [ ] check/review impl,
-;;    - [ ] agree on what continue/render should dispatch on,
-;;    - [ ] make transfer `!` work.
+;;    - [X] check/review impl: Feedback: `continue` is not re-entrant like
+;;      hf/render should be, no way to stop drilling at an arbitrary point in
+;;      the current impl.
+;;    - [X] agree on what continue/render should dispatch on:
+;;      Feedback: it seems ok for now.
+;;
+;;      Considered done for iteration 26. Will refine it later.
 ;;
 
 (ns dustin.hfql.hfql26
@@ -47,16 +53,19 @@
     (reduce-kv (fn [r edge cont]
                  (let [qedge           (qualify env& ns-map edge)
                        path            (conj path qedge)
+                       path'           (conj path '.)
                        edge*           (compile-leaf* edge)
                        [env' edge-sym] (hf-edge->sym! env' edge)]
                    (merge r
                           (if (many? qedge)
                             `{'~qedge
-                              @(reactive-for (~'fn [~'%]
-                                              (dataflow
-                                               @(continue '~path (~'let [~edge-sym ~'%]
-                                                                  ~(compile-hfql* env& ns-map env' path cont)))))
-                                             (~'unquote ~(replace* env' edge*)))}
+                              @(continue '~path
+                                         @(reactive-for (~'fn [~'%]
+                                                         (dataflow
+                                                          @(continue '~path'
+                                                                     (~'let [~edge-sym ~'%]
+                                                                      ~(compile-hfql* env& ns-map env' path' cont)))))
+                                                        (~'unquote ~(replace* env' edge*))))}
                             `{'~qedge
                               (~'let [~'% ~(replace* env' edge*)]
                                @(continue '~path
@@ -74,20 +83,41 @@
  (macroexpand-1 '(hfql {(genders) [:db/ident]}))
  :=
  '{'(dustin.fiddle/genders)
-   @(hfdl.lib/reactive-for
-     (fn
-       [%]
-       (hfdl.lang/dataflow
-        @(dustin.hfql.hfql26/continue
-          '[(dustin.fiddle/genders)]
-          (let
-              [% %]
-            {':db/ident
-             @(dustin.hfql.hfql26/continue
-               '[(dustin.fiddle/genders) :db/ident]
-               (dustin.hfql.hfql25/hf-nav :db/ident %))}))))
-     (unquote (genders)))})
+   @(dustin.hfql.hfql26/continue
+     '[(dustin.fiddle/genders)]
+     @(hfdl.lib/reactive-for
+       (fn
+         [%]
+         (hfdl.lang/dataflow
+          @(dustin.hfql.hfql26/continue
+            '[(dustin.fiddle/genders) .]
+            (let
+                [% %]
+              {':db/ident
+               @(dustin.hfql.hfql26/continue
+                 '[(dustin.fiddle/genders) . :db/ident]
+                 (dustin.hfql.hfql25/hf-nav :db/ident %))}))))
+       (unquote (genders))))})
 
+(defmethod continue '[(dustin.fiddle/submissions needle)] [_ v]
+  (dataflow
+   (into [:table] v) ; because of cardinality N
+   ))
+
+(defmethod continue '[(dustin.fiddle/submissions needle) .] [_ v]
+  (dataflow [:tr
+             (:dustingetz/email v)
+             [:td (get-in v [:dustingetz/gender :db/ident])]
+             [:td (get-in v [:dustingetz/gender '(dustin.fiddle/shirt-sizes dustingetz/gender)])]]))
+
+(defmethod continue '[(dustin.fiddle/submissions needle) . :dustingetz/email] [_ v]
+  (dataflow [:td v]))
+
+(defmethod continue '[(dustin.fiddle/submissions needle) . :dustingetz/gender (dustin.fiddle/shirt-sizes dustingetz/gender)]
+  [_ v]
+  (dataflow (into [:select]
+                  (for [{:keys [db/ident]} v]
+                    [:option ident]))))
 
 (tests
 
@@ -106,43 +136,46 @@
 
  (result program @process)
  :=
- `{(submissions ~'needle)
-   [#:dustingetz{:email "alice@example.com"
-                 :gender
-                 {(shirt-sizes dustingetz/gender)
-                  [#:db{:ident :dustingetz/womens-small}
-                   #:db{:ident :dustingetz/womens-medium}
-                   #:db{:ident :dustingetz/womens-large}]
-                  :db/ident :dustingetz/female}}
-    #:dustingetz{:email "bob@example.com"
-                 :gender
-                 {(shirt-sizes dustingetz/gender)
-                  [#:db{:ident :dustingetz/mens-small}
-                   #:db{:ident :dustingetz/mens-medium}
-                   #:db{:ident :dustingetz/mens-large}]
-                  :db/ident :dustingetz/male}}
-    #:dustingetz{:email "charlie@example.com"
-                 :gender
-                 {(shirt-sizes dustingetz/gender)
-                  [#:db{:ident :dustingetz/mens-small}
-                   #:db{:ident :dustingetz/mens-medium}
-                   #:db{:ident :dustingetz/mens-large}]
-                  :db/ident :dustingetz/male}}]
-   (genders) [#:db{:ident :dustingetz/male}
-              #:db{:ident :dustingetz/female}]}
+ `{(submissions ~'needle) [:table
+                           [:tr
+                            [:td "alice@example.com"]
+                            [:td :dustingetz/female]
+                            [:td
+                             [:select
+                              [:option :dustingetz/womens-small]
+                              [:option :dustingetz/womens-medium]
+                              [:option :dustingetz/womens-large]]]]
+                           [:tr
+                            [:td "bob@example.com"]
+                            [:td :dustingetz/male]
+                            [:td
+                             [:select
+                              [:option :dustingetz/mens-small]
+                              [:option :dustingetz/mens-medium]
+                              [:option :dustingetz/mens-large]]]]
+                           [:tr
+                            [:td "charlie@example.com"]
+                            [:td :dustingetz/male]
+                            [:td
+                             [:select
+                              [:option :dustingetz/mens-small]
+                              [:option :dustingetz/mens-medium]
+                              [:option :dustingetz/mens-large]]]]],
+   (genders)              [#:db{:ident :dustingetz/male}
+                           #:db{:ident :dustingetz/female}]}
 
  (reset! !needle "alice")
 
  (result program @process)
  :=
- `{(submissions ~'needle)
-   [#:dustingetz{:email "alice@example.com"
-                 :gender
-                 {:db/ident :dustingetz/female
-                  (shirt-sizes dustingetz/gender)
-                  [#:db{:ident :dustingetz/womens-small}
-                   #:db{:ident :dustingetz/womens-medium}
-                   #:db{:ident :dustingetz/womens-large}]}}]
-   (genders) [#:db{:ident :dustingetz/male}
-              #:db{:ident :dustingetz/female}]})
-
+ `{(submissions ~'needle) [:table
+                           [:tr
+                            [:td "alice@example.com"]
+                            [:td :dustingetz/female]
+                            [:td
+                             [:select
+                              [:option :dustingetz/womens-small]
+                              [:option :dustingetz/womens-medium]
+                              [:option :dustingetz/womens-large]]]]]
+   (genders)              [#:db{:ident :dustingetz/male}
+                           #:db{:ident :dustingetz/female}]})
