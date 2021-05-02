4edd6e70fdb2d0afb56b4a0f53d9529757b41d04
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2021-05-02 15:13:34 +0200

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: "jit"     // fast incremental build https://tailwindcss.com/docs/just-in-time-mode#watch-mode-and-one-off-builds
+    , prefix: 'tw-' // namespace
+    , purge: [      // filter all classes not in these files
+        './src/**/*.{cljc,cljs,js}'
+        , './resources/public/**/*.{html,svg}'
+    ]
+    , darkMode: false // or 'media' or 'class'
+    , theme: {
+        extend: {}
+    }
+    , variants: {
+        extend: {}
+    }
+    , plugins: []
+}
