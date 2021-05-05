74d56b934e4735f330e6f9f8e3eafcd0dc9fc48d
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2021-05-05 11:52:11 +0200

    Add EDN view spec checking

--- src/hyperfiddle/client/edn_view/linter.cljs
+++ src/hyperfiddle/client/edn_view/linter.cljs
@@ -1,5 +1,7 @@
 (ns hyperfiddle.client.edn-view.linter
-  (:require ["@codemirror/lint" :refer [linter]]
+  (:require ["./spec" :refer [explain]]
+            ["@codemirror/lint" :refer [linter]]
+            [clojure.spec.alpha :as s]
             [edamame.core :refer [parse-string]]))
 
 (defn locate [^js state, err]
@@ -16,24 +18,39 @@
                 :edamame/opened-delimiter]} (ex-data err)
         message                             (ex-message err)
         {:keys [:from :to]} (locate state err)]
-    #js[#js{:from     from
-            :to       to
-            :severity (case type
-                        :edamame/error "error")
-            :source   "EDN Linter"
-            :message  (cond
-                        expected-delimiter (str "Expected " expected-delimiter
-                                                " to match " opened-delimiter)
-                        :else message)}]))
+    [#js{:from     from
+         :to       to
+         :severity (case type
+                     :edamame/error "error")
+         :source   "EDN Linter"
+         :message  (cond
+                     expected-delimiter (str "Expected " expected-delimiter
+                                             " to match " opened-delimiter)
+                     :else message)}]))
 
 (defn lint [^js state]
   (try
     (parse-string (.. state -doc (toString)))
-    #js[]
+    []
     (catch :default e
       (doto (err->diagnostics state e)
         js/console.log))))
 
 
+(defn explainf [sexpr]
+  (try
+    (let [edn    (parse-string sexpr)
+          result (cond
+                   (list? edn) (let [[f & args] edn]
+                                 (when (symbol? f)
+                                   (when-let [args-spec (:args (s/get-spec f))]
+                                     (s/explain-str args-spec args))))
+                   :else       nil)]
+      (when-not (= "Success!\n" result)
+        result))
+    (catch :default _ nil)))
+
 (defonce Linter (linter (fn [^js view]
-                          (lint (.-state view)))))
+                          (let [lint-errors (lint (.-state view))
+                                spec-errors (array-seq (explain (.-state view) explainf))]
+                            (into-array (concat lint-errors spec-errors))))))
