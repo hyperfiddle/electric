1332401ee82ba5a8376d54ad04c1abb7072bca36
Léo NOEL <contact@leonoel.fr>
2021-05-21 09:38:05 +0200

    rfor with support for deferred initialization

--- src/hfdl/impl/rfor.cljs
+++ src/hfdl/impl/rfor.cljs
@@ -1,13 +1,6 @@
 (ns hfdl.impl.rfor
-  (:require [hfdl.impl.util :as u])
-  (:import (clojure.lang IFn IDeref)
-           (java.util.concurrent.atomic AtomicReference AtomicInteger)
-           (java.util.function IntBinaryOperator)))
+  (:require-macros [hfdl.impl.util :as u]))
 
-;; outer: prev, iter, k->b, s->v, queue, ready, notifier, terminator, continuation
-;; branch: prev, iter, slot
-;; patch: prev, barrier, adds, movs, rets
-;; undefined behavior: empty child flows, failing flows, when items in parent collection are not distinct
 (def rfor
   (let [prev (int 0)
         iterator (int 1)
@@ -22,31 +15,28 @@
         barrier (int 1)
         adds (int 2)
         movs (int 3)
-        rets (int 4)
-        toggle (reify IntBinaryOperator (applyAsInt [_ x y] (bit-xor x y)))]
+        rets (int 4)]
     (letfn [(push! [o x]
-              (let [q ^AtomicReference (aget o queue)]
-                (loop []
-                  (let [e (.get q)]
-                    (if (.compareAndSet q (aset x prev e) x)
-                      (when (number? e) ((aget o notifier))) (recur))))))
+              (let [e (aget o queue)]
+                (aset x prev e)
+                (aset o queue x)
+                (when (number? e)
+                  ((aget o notifier)))))
             (done! [o x]
-              (let [q ^AtomicReference (aget o queue)]
-                (aset x iterator nil)
-                (loop []
-                  (let [e (.get q)]
-                    (if (number? e)
-                      (if (.compareAndSet q e (dec e))
-                        (when (= e 1) ((aget o terminator))) (recur))
-                      (if (.compareAndSet q (aset x prev e) x)
-                        (do) (recur)))))))
+              (aset x iterator nil)
+              (let [e (aget o queue)]
+                (if (number? e)
+                  (do (aset o queue (dec e))
+                      (when (= e 1) ((aget o terminator))))
+                  (do (aset x prev e)
+                      (aset o queue x)))))
             (more! [o]
-              (when (zero? (.accumulateAndGet ^AtomicInteger (aget o ready) (int 1) toggle))
+              (when (aset o ready (not (aget o ready)))
                 (if-some [c (aget o continuation)]
                   (let [ks @(aget o iterator)               ;; TODO errors
                         p (object-array 5)]
                     (doto p
-                      (aset barrier (AtomicInteger.))
+                      (aset barrier 0)
                       (aset adds {})
                       (aset movs {})
                       (aset rets (aget o store)))
@@ -65,7 +55,7 @@
                                       (aset iterator
                                         ((c k)
                                          #(if (identical? b (aget b prev))
-                                            (when (zero? (.decrementAndGet ^AtomicInteger (aget p barrier)))
+                                            (when (zero? (aset p barrier (dec (aget p barrier))))
                                               (push! o p) (more! o)) (push! o b))
                                          #(if (identical? b (aget b prev))
                                             (comment TODO child flow is empty, do something)
@@ -78,7 +68,7 @@
                           (aget o store)
                           (aget p adds))
                         (keys (aget p rets))))
-                    (when (zero? (.addAndGet ^AtomicInteger (aget p barrier) (count (aget p adds))))
+                    (when (zero? (aset p barrier (+ (aget p barrier) (count (aget p adds)))))
                       (push! o p) (recur o)))
                   (done! o o))))
             (pull! [o l e]
@@ -100,7 +90,7 @@
                     (do
                       (if-some [s (aget e slot)]
                         (aset o result (assoc (aget o result) s @i)) ;; TODO errors
-                        (try @i (catch Throwable _)))
+                        (try @i (catch :default _)))
                       (if (nil? p) l (recur o l p))))
                   (let [l (dec l)]
                     (if (nil? p) l (recur o l p))))))]
@@ -109,8 +99,8 @@
           (let [o (object-array 9)]
             (doto o
               (aset continuation cont)
-              (aset ready (AtomicInteger.))
-              (aset queue (AtomicReference. 1))
+              (aset ready true)
+              (aset queue 1)
               (aset notifier n)
               (aset terminator t)
               (aset iterator (flow #(more! o) #(doto o (aset continuation nil) (more!))))
@@ -119,41 +109,22 @@
             (more! o)
             (reify
               IFn
-              (invoke [_]
+              (-invoke [_]
                 ;; TODO
                 )
               IDeref
-              (deref [_]
-                (let [q ^AtomicReference (aget o queue)]
-                  (loop [tail nil
-                         live 0]
-                    (let [head (.get q)
-                          live (loop [l live n nil e head]
-                                 (let [p (u/aget-aset e prev n)]
-                                   (if (number? p)
-                                     (pull! o (+ l p) e)
-                                     (if (identical? tail p)
-                                       (pull! o l e) (recur l e p)))))]
-                      (if (.compareAndSet q head live)
-                        (when (zero? live) ((aget o terminator))) (recur head live)))))
+              (-deref [_]
+                (loop [tail nil
+                       live 0]
+                  (let [head (aget o queue)
+                        live (loop [l live n nil e head]
+                               (let [p (u/aget-aset e prev n)]
+                                 (if (number? p)
+                                   (pull! o (+ l p) e)
+                                   (if (identical? tail p)
+                                     (pull! o l e) (recur l e p)))))]
+                    (if (identical? (aget o queue) head)
+                      (do (aset o queue live)
+                          (when (zero? live) ((aget o terminator))))
+                      (recur head live))))
                 (aget o result)))))))))
-
-(comment
-  (require '[missionary.core :as m])
-  (def >control (m/watch (doto (atom [1 2 3]) (->> (def !)))))
-  (def >control (m/observe #(do (def ! %) u/nop)))
-  (def !targets (repeatedly #(atom 0)))
-  (def it
-    ((rfor (fn [x] (m/watch (nth !targets x))) >control)
-     #(prn :ready) #(prn :done)))
-
-  (! [2])
-  @it
-  (! [])
-
-  (swap! (nth !targets 2) inc)
-  (swap! (nth !targets 3) inc)
-
-  (def it ((rfor (fn [_]) (m/ap ())) #(prn :ready) #(prn :done)))
-
-  )
