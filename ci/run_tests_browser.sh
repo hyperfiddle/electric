1cf7f3d037d7b3e2a755bafaad0a1541c4390baa
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-04-19 15:20:56 +0200

    `tests_browser` and `tests_all` scripts now accept extra karma args
    E.g. passing `--browsers Chrome` to either scripts will open a chrome window (not headless)

--- ci/run_tests_browser.sh
+++ ci/run_tests_browser.sh
@@ -2,4 +2,4 @@
 
 echo "Running Browser tests"
 ./node_modules/.bin/shadow-cljs -A:test compile :browser-test
-./node_modules/.bin/karma start --single-run
+./node_modules/.bin/karma start --single-run $@
