cf10bc0b0dcb937e37c14e813b80c3e52e12bc2c
Dustin Getz <dustin.getz@gmail.com>
2022-05-11 15:17:24 -0400

    improve example

--- src-docs/user/photon_call_semanatics.clj
+++ src-docs/user/photon_call_semanatics.clj
@@ -2,19 +2,23 @@
   (:require [hyperfiddle.photon :as p]
             [hyperfiddle.rcf :refer [tests ! % with]]))
 
-(p/defn Foo [n trace!]
+(p/def trace! println)
+
+(p/defn Foo [n]
   (let [x (trace! (+ n 2))]      ; this is computed once, as you'd expect
     (p/fn [y]
       (+ y x))))
 
-(p/defn Bar [c n trace!]
-  (let [F (Foo. c trace!)]
+(p/defn Bar [c n]
+  (let [F (Foo. c)]
     (p/for [x (range n)]
       (F. x))))
 
 (tests
   (def !n (atom 3))
-  (with (p/run (! (Bar. 5 (p/Watch. !n) !)))
+  (with (p/run
+          (binding [trace! !]
+            (! (Bar. 5 (p/watch !n)))))
     % := 7
     % := [7 8 9]
     (swap! !n inc)
