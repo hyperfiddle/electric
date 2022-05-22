eaf76a929da8828f277d7d8fdd58c99ca549d33a
Dustin Getz <dustin.getz@gmail.com>
2022-05-22 11:52:03 -0400

    demo todos is ready, moved to user

--- src-dev/user.cljs
+++ src-dev/user.cljs
@@ -7,11 +7,11 @@
     user.demo-webview
     user.demo-button
     user.demo-counter
+    user.demo-todos-basic
     user.app-starter
     wip.demo-two-clocks
     wip.orders-ui
     wip.demo-hfql
-    wip.demo-todos-basic
     wip.demo-todos-stage
     wip.demo-logical-clock
     ))
