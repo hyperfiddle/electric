9ebb467cd0635cd25a0c9d40f8b82da9a7b711f1
Dustin Getz <dustin.getz@gmail.com>
2022-05-24 14:43:33 -0400

    docs - photon program and equivalent missionary target program

---
+++ scratch/dustin/y2022/photon_compiler_old.cljc
@@ -0,0 +1,65 @@
+(ns dustin.y2022.photon-compiler-old
+  (:require [hyperfiddle.photon :as p]
+            [hyperfiddle.photon-impl.compiler :refer [analyze]]
+            [hyperfiddle.photon-impl.runtime :as r :refer [emit]]
+            [hyperfiddle.rcf :as rcf :refer [tests ! % with]]
+            [missionary.core :as m]))
+
+
+(tests
+
+  (analyze {} '(p/fn [n]
+                 (let [x (+ n 2)]
+                   (p/fn [y]
+                     (+ y x)))))
+
+  ; tuple of [client, server]
+  := [[:constant
+       [:pub
+        [:node 0]
+        [:pub
+         [:apply [:global :clojure.core/+] [:sub 1] [:literal 2]]
+         [:constant [:pub [:node 0] [:apply [:global :clojure.core/+] [:sub 1] [:sub 2]]]]]]]
+      ; this is a nil server program
+      [:target [:target [:nop] [:nop]] [:literal nil]]]
+
+  ; p/fn is split into a constant + target (Yes p/fn, not reactive-quote)
+  ; a target is a recipe to construct a piece of DAG that is constructed on demand by the remote peer
+
+  (def client (first *1))
+  (def server (second *2))
+
+  (emit (comp symbol str) client) :=
+  `(r/peer
+     1 0 0 0 0 0 0
+     (clojure.core/fn [~'nodes]
+       (r/steady
+         (r/constant
+           0 0 0 0 2 0 0
+           (clojure.core/fn [~'nodes]
+             (clojure.core/let [~'pub0 (r/signal 0 (clojure.core/nth ~'nodes 0))]
+               (clojure.core/let [~'pub1 (r/signal 1 (r/latest-apply (r/steady clojure.core/+) ~'pub0 (r/steady (quote 2))))]
+                 (r/steady
+                   (r/constant
+                     0 0 0 0 1 0 0
+                     (clojure.core/fn [~'nodes]
+                       (clojure.core/let [~'pub2 (r/signal 0 (clojure.core/nth ~'nodes 0))]
+                         (r/latest-apply (r/steady clojure.core/+) ~'pub2 ~'pub1))))))))))))
+
+  ; this is a noop server, our current implementation produces clients and servers with matching closure structures
+  ; so the closure IDs always align
+  (emit (comp symbol str) server) :=
+  `(r/peer
+     0 0 1 0 0 0 0
+     (clojure.core/fn [~'nodes]
+       (do
+         (r/target
+           0 0 1 0 0 0 0
+           (clojure.core/fn [~'nodes]
+             (do (r/target
+                   0 0 0 0 0 0 0
+                   (clojure.core/fn [~'nodes]
+                     nil)) nil)))
+         (r/steady (quote nil)))))
+
+  )
--- src-docs/user/photon_compiler.clj
+++ src-docs/user/photon_compiler.clj
@@ -1,63 +1,43 @@
-(ns user.photon_compiler
+(ns user.photon-compiler
   (:require [hyperfiddle.photon :as p]
             [hyperfiddle.photon-impl.compiler :refer [analyze]]
             [hyperfiddle.photon-impl.runtime :as r :refer [emit]]
-            [hyperfiddle.rcf :refer [tests]]))
+            [hyperfiddle.rcf :as rcf :refer [tests ! % with]]
+            [missionary.core :as m]))
+
+; I'm also not sure what can p/fn and p/defn take as arguments, must they be continuous flows?
+
+(hyperfiddle.rcf/enable!)
+
+; Is there a missionary operation one can equivalently rewrite as a p/fn?
+; Like instead of doing an m/eduction one brings the data into the photon side of world and does the same there?
+
+(tests "Photon baseline program - a dataflow diamond"
+  (def !x (atom 0))
+  (def dispose (p/run
+                 (let [x (p/watch !x)
+                       a (inc x)]
+                   (rcf/! (+ a (inc x))))))
+  % := 2
+  (swap! !x inc)
+  % := 4
+  (swap! !x inc)
+  % := 6
+  (dispose))
 
 (tests
-
-  (analyze {} '(p/fn [n]
-                 (let [x (+ n 2)]
-                   (p/fn [y]
-                     (+ y x)))))
-
-  ; tuple of [client, server]
-  := [[:constant
-       [:pub
-        [:node 0]
-        [:pub
-         [:apply [:global :clojure.core/+] [:sub 1] [:literal 2]]
-         [:constant [:pub [:node 0] [:apply [:global :clojure.core/+] [:sub 1] [:sub 2]]]]]]]
-      ; this is a nil server program
-      [:target [:target [:nop] [:nop]] [:literal nil]]]
-
-  ; p/fn is split into a constant + target (Yes p/fn, not reactive-quote)
-  ; a target is a recipe to construct a piece of DAG that is constructed on demand by the remote peer
-
-  (def client (first *1))
-  (def server (second *2))
-
-  (emit (comp symbol str) client) :=
-  `(r/peer
-     1 0 0 0 0 0 0
-     (clojure.core/fn [~'nodes]
-       (r/steady
-         (r/constant
-           0 0 0 0 2 0 0
-           (clojure.core/fn [~'nodes]
-             (clojure.core/let [~'pub0 (r/signal 0 (clojure.core/nth ~'nodes 0))]
-               (clojure.core/let [~'pub1 (r/signal 1 (r/latest-apply (r/steady clojure.core/+) ~'pub0 (r/steady (quote 2))))]
-                 (r/steady
-                   (r/constant
-                     0 0 0 0 1 0 0
-                     (clojure.core/fn [~'nodes]
-                       (clojure.core/let [~'pub2 (r/signal 0 (clojure.core/nth ~'nodes 0))]
-                         (r/latest-apply (r/steady clojure.core/+) ~'pub2 ~'pub1))))))))))))
-
-  ; this is a noop server, our current implementation produces clients and servers with matching closure structures
-  ; so the closure IDs always align
-  (emit (comp symbol str) server) :=
-  `(r/peer
-     0 0 1 0 0 0 0
-     (clojure.core/fn [~'nodes]
-       (do
-         (r/target
-           0 0 1 0 0 0 0
-           (clojure.core/fn [~'nodes]
-             (do (r/target
-                   0 0 0 0 0 0 0
-                   (clojure.core/fn [~'nodes]
-                     nil)) nil)))
-         (r/steady (quote nil)))))
-
-  )
+  "Equivalent dataflow diamond as missionary"
+  (def !x (atom 0))
+  (defn user []
+    (let [<x (m/signal! (m/watch !x))
+          <a (m/signal! (m/latest inc <x))]
+      (m/latest rcf/! (m/latest + <a (m/latest inc <x)))))
+  (def main (m/reactor (m/stream! (user))))                 ; entrypoint that runs for effect
+  (def dispose (main (fn [v] (rcf/! [::success v]))         ; start process, will run until disposed
+                     (fn [err] (rcf/! [::failure err]))))
+  % := 2
+  (swap! !x inc)
+  % := 4
+  (swap! !x inc)
+  % := 6
+  (dispose))
