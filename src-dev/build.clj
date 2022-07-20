6a0b9c455fb35d65a9edc0072857f19baa4a6c2e
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-07-20 18:15:35 +0200

    Add build info

--- src-dev/build.clj
+++ src-dev/build.clj
@@ -1,11 +1,25 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
-
-(defn clean [_]
-  (b/delete {:path "target"}))
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
 
 (defn compile-java [_]
   (b/javac {:src-dirs ["src"]
             :class-dir "src"
             :basis (b/create-basis {:project "deps.edn"})
             :javac-opts ["-source" "8" "-target" "8"]}))
+
+(def defaults
+  {:src-pom "pom.xml"
+   :lib     'com.hyperfiddle/photon})   ; maven group-id and library name
+
+(defn clean [opts]
+  (bb/clean opts))
+
+(defn jar [opts]
+  (bb/jar (merge defaults opts)))
+
+(defn install [opts]
+  (bb/install (merge defaults opts)))
+
+(defn deploy [opts]
+  (bb/deploy (merge defaults opts)))
