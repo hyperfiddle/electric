13eb4cb6bbb07ca75e3b7992ff45d2c16b659dff
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-07-21 18:47:32 +0200

    Drop `server` dev ns
    Replaced by hyperfiddle.photon-jetty-server.
    This namespace depends on ring.adapter.jetty9 and is intended to be loaded only
    if the dependency is satisfied by dependent project.

--- src/hyperfiddle/photon_jetty_server.clj
+++ src/hyperfiddle/photon_jetty_server.clj
@@ -1,11 +1,18 @@
-(ns server
+(ns hyperfiddle.photon-jetty-server
   (:require [ring.middleware.file :refer [wrap-file]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
             [ring.adapter.jetty9 :as ring]
             [hyperfiddle.photon-jetty-adapter :as adapter]
             [hyperfiddle.logger :as log])
   (:import [java.io IOException]
            [java.net BindException]
            ))
+
+(defn wrap-default-page [next-handler]
+  (fn [ring-req]
+    (case (:uri ring-req)
+      ("" "/") (next-handler (assoc ring-req :uri "/index.html"))
+      (next-handler ring-req))))
 
 #_
 (defn wrap-photon [next-handler]        ; Jetty 10 allows such handler
@@ -21,7 +28,9 @@
 (defn start-server! [config]
   (try
     (ring/run-jetty (-> default-handler
-                      (wrap-file "resources/public")
+                      (wrap-file (or (:resources-path config) "resources/public"))
+                      (wrap-content-type)
+                      (wrap-default-page)
                       #_(wrap-photon))
       ;; Jetty 9 forces us to declare WS paths out of a ring handler.
       (merge {:port       8080
