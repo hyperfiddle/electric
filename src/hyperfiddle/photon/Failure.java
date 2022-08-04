6d0e3eb6821bcc6b5bf7c58fb401f9ef5097da9a
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-08-04 15:03:30 +0200

    Define equality for Failure and Pending

--- src/hyperfiddle/photon/Failure.java
+++ src/hyperfiddle/photon/Failure.java
@@ -4,4 +4,7 @@ public class Failure {
     public Failure(Throwable e) {
         this.error = e;
     }
+    public boolean equals(Object o){
+        return (o instanceof Failure) && (this.error.equals(((Failure) o).error));
+    }
 }
--- src/hyperfiddle/photon/Pending.java
+++ src/hyperfiddle/photon/Pending.java
@@ -1,2 +1,6 @@
 package hyperfiddle.photon;
-public class Pending extends Throwable {}
+public class Pending extends Throwable {
+    public boolean equals(Object o){
+        return (o instanceof Pending);
+    }
+}
