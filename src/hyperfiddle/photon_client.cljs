a952a911980dbee6757a15f0082c3d04bd975708
Léo NOEL <contact@leonoel.fr>
2022-08-18 14:40:44 +0200

    temporary fix for peer shutdown process

--- src/hyperfiddle/photon_client.cljs
+++ src/hyperfiddle/photon_client.cljs
@@ -33,30 +33,29 @@
 
 (defn make-write-chan [socket mailbox]
   (.addEventListener socket "message" #(mailbox (.-data %)))
-  (fn [value]
-    (fn [success failure]
-      (try
-        (log/trace "🔼" value)
-        (.send socket value)
-        (success nil)
-        (catch :default e
-          (log/error "Failed to write on socket" e)
-          (failure e)))
-      #(log/info "Canceling websocket write"))))
+  (fn
+    ([] (.close socket 1000 "graceful shutdown"))
+    ([value]
+     (fn [success failure]
+       (try
+         (log/trace "🔼" value)
+         (.send socket value)
+         (success nil)
+         (catch :default e
+           (log/error "Failed to write on socket" e)
+           (failure e)))
+       #(log/info "Canceling websocket write")))))
 
 (defn connect [mailbox ws-server-url]
   (m/ap (loop [] (if-let [socket (m/? (connect! (new js/WebSocket ws-server-url)))]
                    (m/amb (do (some-> js/document (.getElementById "root") (.setAttribute "data-ws-connected" "true"))
                             (make-write-chan socket mailbox))
                      (do (log/info "WS Waiting for close…")
-                       (when (try (let [[code reason] (m/? (wait-for-close socket))]
-                                    (case code
-                                      1011 (do (log/error "WS closed" code reason)
-                                               false)
-                                      true))
-                                  (catch Cancelled _ ; process is cancelled, client should gracefully close socket
-                                    (.close socket 1000 "gracefull shutdown")
-                                    false))
+                       (when (let [[code reason] (m/? (wait-for-close socket))]
+                               (case code
+                                 1011 (do (log/error "WS closed" code reason)
+                                          false)
+                                 true))
                          (some-> js/document (.getElementById "root") (.setAttribute "data-ws-connected" "false"))
                          (log/info "WS Closed.")
                          (log/info "WS Retry in 2 seconds…")
@@ -70,13 +69,13 @@
 (def ^:dynamic *ws-server-url* (server-url))
 
 (defn client [client server]
-  (m/reduce {} nil (m/ap
-                     (try
-                       (let [mailbox (m/mbx)]
-                         (if-let [write-chan (m/?< (connect mailbox *ws-server-url*))]
-                           (do (log/info "Starting Photon Client")
-                               (m/? (write-chan (io/encode server))) ; bootstrap server
-                               (m/? (client (io/message-writer write-chan)
-                                      (io/message-reader mailbox))))
-                           (log/info "Stopping Photon Client")))
-                       (catch Cancelled _ "stopped")))))
+  (m/reduce {} nil
+    (m/ap
+      (let [mailbox (m/mbx)]
+        (if-let [write-chan (m/?< (connect mailbox *ws-server-url*))]
+          (try (log/info "Starting Photon Client")
+               (m/? (write-chan (io/encode server)))        ; bootstrap server
+               (m/? (client (io/message-writer write-chan)
+                      (io/message-reader mailbox)))
+               (finally (write-chan)))
+          (log/info "Stopping Photon Client"))))))
