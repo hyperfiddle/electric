e0ccb7468d1f8fd9151f6b3ae974cab84a590be7
Dustin Getz <dustin.getz@gmail.com>
2022-08-25 10:58:29 -0400

    abstract explorer split out from demo folder explorer

--- src-docs/user/datafy_fs.clj
+++ src-docs/user/datafy_fs.clj
@@ -1,9 +1,9 @@
 (ns user.datafy-fs
   "nav implementation for java file system traversals"
-  (:require [hyperfiddle.rcf :refer [tests]]
+  (:require [clojure.core.protocols :as ccp :refer [nav]]
             [clojure.datafy :refer [datafy]]
-            [clojure.core.protocols :as ccp :refer [nav]]
-            clojure.repl)
+            [clojure.spec.alpha :as s]
+            [hyperfiddle.rcf :refer [tests]])
   (:import java.nio.file.Paths
            java.nio.file.Files
            java.io.File
@@ -11,14 +11,46 @@
            java.nio.file.attribute.BasicFileAttributes
            java.nio.file.attribute.FileTime))
 
-(def ... `...) ; resolve to symbol for test assertions
+; spec the data, not the object
+(s/def ::name string?)
+(s/def ::absolute-path string?)
+(s/def ::modified inst?)
+(s/def ::created inst?)
+(s/def ::accessed inst?)
+(s/def ::size string?)
+(s/def ::kind qualified-keyword?) ; extension as well?
+(s/def ::file (s/keys :opt [::name ::absolute-path ::modified ::created ::accessed ::size ::kind]))
+(s/def ::children (s/coll-of ::file))
+
+(defn get-extension [path]
+  (let [found (last (re-find #"(\.[a-zA-Z0-9]+)$" path))
+        ext (and found (subs found 1))]
+    (or ext nil)))
+
+(tests
+  "get-extension"
+
+  (tests
+    "empty"
+    (get-extension "") := nil
+    (get-extension ".") := nil
+    (get-extension "..") := nil
+    (get-extension "image") := nil
+    (get-extension "image.") := nil
+    (get-extension "image..") := nil)
+
+  (tests
+    "found"
+    (get-extension "image.png") := "png"
+    (get-extension "image.blah.png") := "png"
+    (get-extension "image.blah..png") := "png"))
 
 (defn file-attrs [h]
-  (-> h
-      .getAbsolutePath
+  (-> h .getAbsolutePath
       (java.nio.file.Paths/get (make-array String 0))
-      (java.nio.file.Files/readAttributes java.nio.file.attribute.BasicFileAttributes
-                                          (make-array java.nio.file.LinkOption 0))))
+      (java.nio.file.Files/readAttributes
+        java.nio.file.attribute.BasicFileAttributes
+        (make-array java.nio.file.LinkOption 0))))
 
 (comment
   (def h (clojure.java.io/file "src/"))
@@ -37,59 +69,80 @@
 
 (extend-protocol ccp/Datafiable
   java.io.File
-  (datafy [o]
-    ; represent object as EDN-ready data, partially (e.g. one level deep).
-    ; datafy is partial print with ability to resolve further via lazy links
-    (let [m (file-attrs o)]
-      (-> (merge {::name (.getName o)
-                  #_#_::creationTime (-> m .creationTime .toInstant java.util.Date/from)
-                  #_#_::lastAccessTime (-> m .lastAccessTime .toInstant java.util.Date/from)
-                  ::lastModifiedTime (-> m .lastModifiedTime .toInstant java.util.Date/from)
-                  ;::isRegularFile (if-not (.isRegularFile m) :not-a-regular-file)
-                  ;::isSymbolicLink (if (.isSymbolicLink m) :link)
-                  ;::isOther (if (.isOther m) :other nil)
-                  ::size (.size m)}
-                 (if (.isDirectory m)
-                   {::isDirectory (if (.isDirectory m) ::folder ::file)
-                    ::children (lazy-seq (.listFiles o))}))
-          (with-meta {`ccp/nav
-                      (fn [xs k v]
-                        (case k
-                          ::lastModifiedTime (.lastModifiedTime m)
-                          ::children (vec v)
-                          v))})))))
+  (datafy [file]
+    ; represent object's top layer as EDN-ready value records, for display
+    ; datafy is partial display view of an object as value records
+    ; nav is ability to resolve back to the underlying object pointers
+    ; they compose to navigate display views of objects like a link
+    (let [attrs (file-attrs file)
+          n (.getName file)]
+      (as-> {::name n
+             ::kind (cond (.isDirectory attrs) ::dir
+                          (.isSymbolicLink attrs) ::symlink
+                          (.isRegularFile attrs) (if-let [s (get-extension n)]
+                                                   (keyword (namespace ::foo) s)
+                                                   ::unknown-file-type)
+                          (.isOther attrs) ::other)
+             ::absolute-path (-> file .getAbsolutePath)
+             ::created (-> attrs .creationTime .toInstant java.util.Date/from)
+             ::accessed (-> attrs .lastAccessTime .toInstant java.util.Date/from)
+             ::modified (-> attrs .lastModifiedTime .toInstant java.util.Date/from)
+             ::size (.size attrs)} %
+            (merge % (if (= ::dir (::kind %))
+                       {::children (lazy-seq (.listFiles file))}))
+            (with-meta % {`ccp/nav
+                          (fn [xs k v]
+                            (case k
+                              ; reverse data back to object, to be datafied again by caller
+                              ::modified (.lastModifiedTime attrs)
+                              ::created (.creationTime attrs)
+                              ::accessed (.lastAccessTime attrs)
+                              ::children (some-> v vec)
+                              v))})))))
 
 (tests
   ; careful, calling seq loses metas on the underlying
   (def h (clojure.java.io/file "src/"))
   (type h) := java.io.File
+  "(datafy file) returns an EDN-ready data view that is one layer deep"
   (datafy h)
   := #:user.datafy-fs{:name "src",
-                      :lastModifiedTime _,
+                      :absolute-path _,
                       :size _,
-                      :isDirectory ::folder,
+                      :modified _,
+                      :created _,
+                      :accessed _,
+                      :kind ::dir,
                       :children _}
+
+  "datafy of a directory includes a Clojure coll of children, but child elements are native file
+  objects"
   (as-> (datafy h) %
         (nav % ::children (::children %))
         (datafy %)
-        (map type %))
+        (take 2 (map type %)))
   := [java.io.File java.io.File]
 
   "nav to a leaf returns the native object"
   (as-> (datafy h) %
-        (nav % ::lastModifiedTime (::lastModifiedTime %)))
+        (nav % ::modified (::modified %)))
   (type *1) := java.nio.file.attribute.FileTime
 
   "datafy again to get the plain value"
   (type (datafy *2)) := java.util.Date
 
-  (as-> (datafy h) <>
-        (nav <> ::children (::children <>))
-        (datafy <>) ; can skip - simple data
-        (nav <> 0 (<> 0))
-        (datafy <>))
+  (as-> (datafy h) %
+        (nav % ::children (::children %))
+        (datafy %) ; can skip - simple data
+        (nav % 0 (% 0))
+        (datafy %)
+        #_(s/conform ::file %))
   := #:user.datafy-fs{:name "hyperfiddle",
-                      :lastModifiedTime _,
+                      :absolute-path _,
                       :size _,
-                      :isDirectory ::folder,
-                      :children _})
+                      :modified _,
+                      :created _,
+                      :accessed _,
+                      :kind ::dir,
+                      :children _}
+  nil)
--- src-docs/user/demo_7_explorer.cljc
+++ src-docs/user/demo_7_explorer.cljc
@@ -1,77 +1,32 @@
 (ns user.demo-7-explorer
-  (:require [clojure.datafy :refer [datafy]]
-            [clojure.core.protocols :refer [nav]]
-            #?(:clj [user.datafy-fs :as fs]
-               :cljs [user.datafy-fs :as-alias fs])
-            [hyperfiddle.photon :as p]
+  (:require [hyperfiddle.photon :as p]
             [hyperfiddle.photon-dom :as dom]
             [hyperfiddle.photon-ui :as ui]
-            [user.util :refer [includes-str? pprint-str]])
+            [hyperfiddle.explorer :as explorer :refer [Explorer]]
+            [user.datafy-fs #?(:clj :as :cljs :as-alias) fs])
   #?(:cljs (:require-macros user.demo-7-explorer)))
 
-; all explorer bindings and p/fns must be called from server
-(p/def indent-level (p/client 0))
-(p/defn Indent [Xs]
-  (p/client
-    (binding [indent-level (inc indent-level)]
-      ; top-down render pass
-      (p/server (p/for [X Xs]
-                  (X.))))))
-
-#?(:cljs (defn format-value [k v]
-           (case k
-             ::fs/lastModifiedTime (.toLocaleDateString v)
-             (str v))))
-
-(p/defn Render-row [m]
-  (p/client
-    (dom/tr
-      (dom/td {:style {:padding-left (-> indent-level (* 10) (str "px"))}}
-        (p/server (::fs/name m)))
-      (p/server
-        (do (p/for [[k v] m]
-              (p/client (dom/td (format-value k v))))
-            nil)))))
-
-(p/def Tree)
-(p/defn Explorer [h]
-  (assert (.isDirectory h) (str "file handle: " (.getName h) " is not a directory"))
-  (p/client
-    (let [!search (atom "") search (p/watch !search)]
-      (dom/div {:class "photon-demo-explorer"}
-        (dom/div {:class "title"} (p/server (.getAbsolutePath h)))
-        (ui/input {::dom/placeholder "Search files by name" ::dom/type "search"
-                   ::ui/input-event (p/fn [e] (reset! !search (.. e -target -value)))})
-        (dom/hr)
-        (dom/table
-          (p/server
-            (binding [Tree (p/fn [m] ; recur via binding until Photon gets proper recursion syntax
-                             (->> (p/for [x (datafy (nav m ::fs/children (::fs/children m)))]
-                                    (let [m (datafy x)]
-                                      (if (::fs/children m)
-                                        (let [Xs (Tree. m)] ; prewalk
-                                          (when (seq Xs)
-                                            ; don't render bottom up; thunk it for later top-down pass
-                                            (p/fn []
-                                              (Render-row. m) ; omit folder row if no descendents matched filter
-                                              (Indent. Xs))))
-                                        (when (includes-str? (::fs/name m) search) ; return nil to remove level
-                                          (p/fn [] (Render-row. m))))))
-                                  (remove nil?)))]
-
-              ;(Render-row. h) -- never render target folder
-              (if-let [Xs (seq (Tree. (datafy h)))]
-                (Indent. Xs)
-                (p/client (dom/div "no results matched"))))))))))
-
-(def !target #?(:cljs (atom "src") :clj nil)) (p/def target (p/client (p/watch !target)))
+(def !target #?(:cljs (atom "src") :clj nil))
 
 (p/defn App []
   (dom/div
-    (dom/h1 "Folder Explorer")
+    (dom/h1 "Explorer")
     (dom/link {:rel :stylesheet, :href "user_demo_explorer.css"})
     #_(dom/div "Folder: " (ui/button {::ui/click-event (p/fn [e] (reset! !target "node_modules"))} "node_modules"))
-    (p/server (Explorer. (clojure.java.io/file (p/client target))))))
+    (p/server
+      (binding
+        [explorer/Title (p/fn [m] (::fs/absolute-path m))
+         ;explorer/Render-row (p/fn [m])
+         explorer/cols [::fs/name ::fs/modified ::fs/size ::fs/kind]
+         explorer/children ::fs/children
+         explorer/search-attr ::fs/name
+         explorer/Format (p/fn [k v]
+                           (p/client
+                             (case k
+                               ::fs/modified (.toLocaleDateString v)
+                               ::fs/kind (name v)
+                               (str v))))]
+        (Explorer. (clojure.java.io/file (p/client (p/watch !target))))))))
 
 ; Improvements
 ; Native search
