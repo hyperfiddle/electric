252f53849bada048780a883b7132a8409b9967a8
Dustin Getz <dustin.getz@gmail.com>
2022-08-27 15:25:45 -0400

    datafy-fs - directory ::parent nav

--- src-docs/user/datafy_fs.clj
+++ src-docs/user/datafy_fs.clj
@@ -4,12 +4,10 @@
             [clojure.datafy :refer [datafy]]
             [clojure.spec.alpha :as s]
             [hyperfiddle.rcf :refer [tests]])
-  (:import java.nio.file.Paths
-           java.nio.file.Files
+  (:import [java.nio.file Path Paths Files]
            java.io.File
            java.nio.file.LinkOption
-           java.nio.file.attribute.BasicFileAttributes
-           java.nio.file.attribute.FileTime))
+           [java.nio.file.attribute BasicFileAttributes FileTime]))
 
 ; spec the data, not the object
 (s/def ::name string?)
@@ -45,23 +43,50 @@
     (get-extension "image.blah.png") := "png"
     (get-extension "image.blah..png") := "png"))
 
-(defn file-attrs [h]
-  (-> h .getAbsolutePath
-      (java.nio.file.Paths/get (make-array String 0))
-      (java.nio.file.Files/readAttributes
-        java.nio.file.attribute.BasicFileAttributes
-        (make-array java.nio.file.LinkOption 0))))
-
 (comment
+  "java.io.File interop"
   (def h (clojure.java.io/file "src/"))
-  (.getName h)
-  (.isDirectory h)
-  (.isFile h)
-  (.listFiles h)
-  (for [x (take 5 (.listFiles h))]
-    (.getName x))
-  (.getPath h)
-  (seq (.listFiles h)))
+  (.getName h) := "src"
+  (.getPath h) := "src"
+  (.isDirectory h) := true
+  (.isFile h) := false
+  ;(.getParent h) := nil -- ??
+  ;(.getParentFile h) := nil -- ??
+  (-> (datafy java.io.File) :members keys)
+  (->> (seq (.listFiles h)) (take 1) first datafy)
+  (for [x (take 5 (.listFiles h))] (.getName x)))
+
+(defn file-path [^File f]
+  (-> f .getAbsolutePath (java.nio.file.Paths/get (make-array String 0))))
+
+(tests
+  (def p (file-path (clojure.java.io/file "src")))
+  (instance? Path p) := true
+  (-> (datafy Path) :members keys)
+  (-> p .getRoot str) := "/"
+  (-> p .getFileName str) := "src"
+  (-> p .getParent .getFileName str) := "photon"
+  (-> p .getParent .toFile .getName) := "photon"
+  #_(-> p .getParent .toFile datafy))
+
+(defn path-attrs [^Path p]
+  (Files/readAttributes p BasicFileAttributes (make-array java.nio.file.LinkOption 0)))
+
+(tests
+  (def attrs (path-attrs (file-path (clojure.java.io/file "src"))))
+  (instance? BasicFileAttributes attrs) := true
+  (.isDirectory attrs) := true
+  (.isSymbolicLink attrs) := false
+  (.isRegularFile attrs) := false
+  (.isOther attrs) := false)
+
+(defn file-attrs [^File f] (path-attrs (file-path f)))
+
+(tests
+  (file-attrs (clojure.java.io/file "src"))
+  )
+
+(def ... `...) ; define a value for easy test assertions
 
 (extend-protocol ccp/Datafiable
   java.nio.file.attribute.FileTime
@@ -69,13 +94,13 @@
 
 (extend-protocol ccp/Datafiable
   java.io.File
-  (datafy [file]
+  (datafy [^File f]
     ; represent object's top layer as EDN-ready value records, for display
     ; datafy is partial display view of an object as value records
     ; nav is ability to resolve back to the underlying object pointers
     ; they compose to navigate display views of objects like a link
-    (let [attrs (file-attrs file)
-          n (.getName file)]
+    (let [attrs (file-attrs f)
+          n (.getName f)]
       (as-> {::name n
              ::kind (cond (.isDirectory attrs) ::dir
                           (.isSymbolicLink attrs) ::symlink
@@ -83,13 +108,14 @@
                                                    (keyword (namespace ::foo) s)
                                                    ::unknown-file-type)
                           (.isOther attrs) ::other)
-             ::absolute-path (-> file .getAbsolutePath)
+             ::absolute-path (-> f .getAbsolutePath)
              ::created (-> attrs .creationTime .toInstant java.util.Date/from)
              ::accessed (-> attrs .lastAccessTime .toInstant java.util.Date/from)
              ::modified (-> attrs .lastModifiedTime .toInstant java.util.Date/from)
              ::size (.size attrs)} %
             (merge % (if (= ::dir (::kind %))
-                       {::children (lazy-seq (.listFiles file))}))
+                       {::children (lazy-seq (.listFiles f))
+                        ::parent `...}))
             (with-meta % {`ccp/nav
                           (fn [xs k v]
                             (case k
@@ -98,6 +124,7 @@
                               ::created (.creationTime attrs)
                               ::accessed (.lastAccessTime attrs)
                               ::children (some-> v vec)
+                              ::parent (-> f file-path .getParent .toFile)
                               v))})))))
 
 (tests
@@ -113,8 +140,10 @@
                       :created _,
                       :accessed _,
                       :kind ::dir,
-                      :children _}
+                      :children _
+                      :parent ...})
 
+(tests
   "datafy of a directory includes a Clojure coll of children, but child elements are native file
   objects"
   (as-> (datafy h) %
@@ -129,8 +158,9 @@
   (type *1) := java.nio.file.attribute.FileTime
 
   "datafy again to get the plain value"
-  (type (datafy *2)) := java.util.Date
+  (type (datafy *2)) := java.util.Date)
 
+(tests
   (as-> (datafy h) %
         (nav % ::children (::children %))
         (datafy %) ; can skip - simple data
@@ -144,5 +174,19 @@
                       :created _,
                       :accessed _,
                       :kind ::dir,
-                      :children _}
-  nil)
+                      :children _
+                      :parent ...})
+
+(tests
+  "nav into children and back up via parent ref"
+  (def m (datafy h))
+  (::name m) := "src"
+  (as-> m %
+        (nav % ::children (::children %))
+        (datafy %) ; dir
+        (nav % 0 (get % 0)) ; first file in dir
+        (datafy %)
+        (nav % ::parent (::parent %)) ; dir (skip level on way up)
+        (datafy %)
+        (::name %))
+  := "src")
