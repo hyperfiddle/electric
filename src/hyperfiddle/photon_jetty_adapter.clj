bb88afbc34fcb29b8391630493593700d081d014
Léo NOEL <contact@leonoel.fr>
2022-08-30 16:21:19 +0200

    server reactor shutdown on ws close

--- src/hyperfiddle/photon_jetty_adapter.clj
+++ src/hyperfiddle/photon_jetty_adapter.clj
@@ -35,11 +35,33 @@
   [^SuspendToken token]
   (.resume token))
 
+(defn success [exit-value] (log/debug "Websocket handler completed gracefully." {:exit-value exit-value}))
+(defn failure [^WebSocketAdapter ws ^Throwable e]
+  (log/error "Websocket handler failure" e)
+  ;; jetty/close! is missing arity 3 for jetty 9. Call close directly to get arity 3.
+  (when-some [s (.getSession ws)] (.close s 1011 "Server process crash")))
+
+(defn write-msg
+  "Return a task, writing a message on a websocket when run."
+  [ws message]
+  (fn [s f]
+    (try
+      (jetty/send! ws message {:write-failed  (fn write-failed [err] (f err))
+                               :write-success (fn write-success [] (s :ack))})
+      ;; jetty/send! throws NPE when ws remote is null
+      (catch Throwable e (f e)))
+    #()))
+
+(defn check-cancelled [cancel task]
+  (let [t (m/race cancel task)]
+    (m/sp (if-some [x (m/? t)]
+            x (throw (Cancelled.))))))
+
 (defn photon-ws-adapter
   "Start and manage a photon server process hooked onto a websocket."
   [handler-f]
-  (let [state        (atom {:session   nil ; jetty session object
-                            :cancel!   nil ; a function cancelling the current photon process
+  (let [cancel       (m/dfv)
+        state        (atom {:session   nil ; jetty session object
                             :token     nil ; a session suspend token, used to resume a jetty session
                             :heartbeat nil ; a function cancelling the heartbeat process
                             })
@@ -50,25 +72,29 @@
                                         ; message.
         pong-mailbox (m/mbx)
         resume!      (fn [_] (token-resume! (:token @state)))]
-    {:on-connect (fn on-connect [ws]
+    {:on-connect (fn on-connect [^WebSocketAdapter ws]
                    (log/debug "WS connect" (jetty/req-of ws))
                    (let [session (.getSession ws)]
                      (.setMaxTextMessageSize (.getPolicy session) (* 100 1024 1024))  ; Allow large value paylods, temporary.
+                     ((handler-f
+                        (fn [x]
+                          (check-cancelled cancel
+                            (write-msg ws x)))
+                        (check-cancelled cancel messages))
+                      success (partial failure ws))  ; Start photon process
                      (swap! state assoc
-                       :session   session
-                       :cancel!   (handler-f ws messages)  ; Start photon process
+                       :session session
                        :heartbeat ((make-heartbeat session pong-mailbox) (fn [_]) (fn [_])))))
      :on-close   (fn on-close [ws status-code reason]
-                   (let [status  {:status status-code, :reason reason}]
+                   (let [status {:status status-code, :reason reason}]
                      (case status-code ; https://www.rfc-editor.org/rfc/rfc6455.html#section-7.4.1
                        1000 (log/debug "Client disconnected gracefully" status)
                        1001 (log/debug "Client navigated away" status)
                        ;; 1005 is the default close code set by Chrome an FF unless specified.
-                       1005 (log/debug  "Client disconnected for an unknown reason (browser default close code)" status)
+                       1005 (log/debug "Client disconnected for an unknown reason (browser default close code)" status)
                        (log/debug "Client disconnected for an unexpected reason." status))
                      ((:heartbeat @state)) ; cancel heartbeat
-                     ((:cancel! @state))   ; cancel (terminate) photon process
-                     ))
+                     (cancel nil)))
      :on-error   (fn on-error [ws err]
                    (log/error "Websocket error" err))
      :on-ping    (fn on-ping [ws bytebuffer]) ; Ignore client ping, no use case.
@@ -87,39 +113,17 @@
                    (swap! state assoc :token (session-suspend! (.getSession ws)))
                    ((messages (ByteBuffer/wrap bytes offset length)) resume! resume!))}))
 
-(defn write-msg
-  "Return a task, writing a message on a websocket when run."
-  [ws message]
-  (fn [s f]
-    (jetty/send! ws message {:write-failed  (fn write-failed [err] (f err))
-                             :write-success (fn write-success [] (s nil))})
-    #()))
-
-(defn -silenced-exception? [e]
-  ; silence harmless error to prevent user confusion until fixed
-  ; https://www.notion.so/hyperfiddle/Cannot-invoke-org-eclipse-jetty-websocket-api-RemoteEndpoint-sendString-String-org-eclipse-jetty-w-489a9529bf75444993c23d66f88c7104
-  (and (instance? NullPointerException e)
-       (clojure.string/includes? (.getMessage e) "org.eclipse.jetty.websocket.api.RemoteEndpoint.sendString")))
-
-(defn success [exit-value] (log/debug "Websocket handler completed gracefully." {:exit-value exit-value}))
-(defn failure [^WebSocketAdapter ws ^Throwable e]
-  (when-not (-silenced-exception? e) ; sorry
-    (log/error "Websocket handler failure" e)
-    ;; jetty/close! is missing arity 3 for jetty 9. Call close directly to get arity 3.
-    (when-some [s (.getSession ws)] (.close s 1011 "Server process crash"))))
-
 (defn photon-ws-message-handler
   "Given a websocket instance and a missionary task reading a message, run a photon
   program named by the client. Original HTTP upgrade ring request map is
   accessible using `(ring.adapter.jetty9/req-of ws)`."
-  [ring-req ws read-msg]
+  [ring-req write-msg read-msg]
   (binding [hf/*http-request* ring-req]
     ; Photon can resolve any dynamic var bound at this point
     (let [resolvef (bound-fn [not-found x] (r/dynamic-resolve not-found x))]
-      ((m/sp
-         (try
-           (m/? ((p/eval resolvef (io/decode (m/? read-msg)))            ; read and eval photon program sent by client
-                 (io/message-writer (partial write-msg ws))
-                 (io/message-reader read-msg)))
-           (catch Cancelled _)))
-       success (partial failure ws)))))
+      (m/sp
+        (try
+          (m/? ((p/eval resolvef (io/decode (m/? read-msg)))            ; read and eval photon program sent by client
+                (io/message-writer write-msg)
+                (io/message-reader read-msg)))
+          (catch Cancelled _))))))
