29ea5daac4f75e589fa24643d2cf8816013b3fa7
Dustin Getz <dustin.getz@gmail.com>
2022-09-13 14:12:20 -0400

    entrypoint - extract application main to a CLJC file so we can use p/server

--- src-dev/user.cljs
+++ src-dev/user.cljs
@@ -1,15 +1,10 @@
 (ns ^:dev/always user ; recompile Photon entrypoint when Photon source changes
-  (:require
-    [clojure.string :as str]
-    goog.object
-    [hyperfiddle.photon :as p]
-    [hyperfiddle.photon.debug :as dbg]
-    [hyperfiddle.photon-dom :as dom]
-    hyperfiddle.photon-dom-test
-    hyperfiddle.rcf
-    user.demo-entrypoint)
-  (:import [hyperfiddle.photon Pending]
-           [missionary Cancelled])
+  (:require clojure.string
+            goog.object
+            hyperfiddle.photon
+            hyperfiddle.photon-dom-test
+            hyperfiddle.rcf
+            user-main)
   (:require-macros [user :refer [get-main]]))
 
 (defn runtime-resolve [exported-qualified-sym]
@@ -19,19 +14,8 @@
         path-segments (clojure.string/split path-s ".")]
     (goog.object/getValueByKeys js/window (clj->js path-segments))))
 
-(def ^:export demo-main
-  (p/boot
-    (try
-      (binding [dom/node (dom/by-id "root")]
-        (dom/div {}
-          (user.demo-entrypoint/App.)))
-      (catch Pending _)
-      (catch Cancelled e (throw e))
-      (catch :default err
-        (js/console.error (str (ex-message err) "\n\n" (dbg/stack-trace p/trace)) err)
-        #_(throw err)))))
-
-(defonce user-photon-main (get-main user/demo-main)) ; lazy resolve
+(def ^:export photon-main (hyperfiddle.photon/boot (user-main/Main.)))
+(defonce user-photon-main (get-main user/photon-main)) ; lazy resolve, dev user can alter at REPL
 (defn set-main [s] (set! user-photon-main (symbol s)))
 (defonce reactor nil)
 
