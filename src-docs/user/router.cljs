3b77be8914528eec9db93baa46adc3a5f4aa8bf4
Dustin Getz <dustin.getz@gmail.com>
2022-10-03 12:49:36 -0400

    demo: router rebuild

--- src-docs/user/router.cljs
+++ src-docs/user/router.cljs
@@ -1,64 +1,88 @@
 (ns user.router
   (:require [hyperfiddle.rcf :as rcf :refer [% tests with tap]]
-            [hyperfiddle.event :as event]
-            [missionary.core :as m]))
+            [hyperfiddle.photon :as p]
+            [hyperfiddle.photon-dom :as dom]
+            [hyperfiddle.photon-ui :as ui]
+            [missionary.core :as m])
+  (:import [missionary Cancelled]))
 
-(def route-event-type "hf-routed")
-(defn current-path [] (-> js/window .-location .-pathname))
-(defn ->route-event ([] (->route-event (current-path))) ([path] (event/->event route-event-type path)))
-(defonce popstate-listener
-  (event/listen js/window "popstate" (fn [_] (event/dispatch js/window (->route-event)))))
-(defn route! [path]
-  (.pushState js/window.history nil "" path) (event/dispatch js/window (->route-event path)))
-(defn reroute! [path]
-  (.replaceState js/window.history nil "" path) (event/dispatch js/window (->route-event path)))
-(defn from
-  ([match-fn] (from match-fn current-path))
-  ([match-fn init-fn]
-   (->> (m/observe
-          (fn mount [!]
-            (let [discard (event/listen js/window route-event-type (comp ! event/data))]
-              (! (init-fn))
-              (fn unmount [] (discard)))))
-     (m/eduction (keep match-fn) (dedupe)))))
+(defn mbx->flow [mbx]
+  (m/ap
+    (loop [v (m/? mbx)]
+      (m/amb v (recur (m/? mbx))))))
 
-(tests "basic routing works"
-  (let [router (from {"/" :slash "/foo" :foo} (constantly "/foo")), it (router #(tap ::notified) #(do))]
-    (with it                   % := ::notified  @it := :foo
-      (route! "/")             % := ::notified  @it := :slash
-      (route! "/foo")          % := ::notified  @it := :foo
-      (reroute! "/")           % := ::notified  @it := :slash)))
+(defn pushState! [!route path] (.pushState js/window.history nil "" path) (!route path))
+(defn replaceState! [!route path] (.replaceState js/window.history nil "" path) (!route path))
 
-(tests "values are deduped"
-  (let [router (from {"/" :slash "/foo" :foo} (constantly "/foo")), it (router #(tap ::notified) #(do))]
-    (with it                   % := ::notified  @it := :foo
-      (route! "/")             % := ::notified  @it := :slash
-      (route! "/")                      ; deduped
-      (route! "/foo")          % := ::notified  @it := :foo)))
+(defn path> "discrete flow of location pathname"
+  ; browser differentiates between physical navigation and js navigation, it only calls popstate
+  ; on physical navigation. So we must simulate this event with a mailbox. Note goog.history
+  ; has the same mailbox machinery.
+  ([mbx] (path> mbx (fn [e] (let [loc (-> js/window .-location)]
+                              (str (.-pathname loc) (.-search loc) (.-hash loc))))))
+  ([mbx current-path!]
+   (m/ap
+     (m/amb=
+       ; process to connect back/forward browser events to the router
+       (m/?> (m/observe (fn mount [!]
+                          #_(println ::mount)
+                          (! (current-path!))
+                          (let [f (fn [e] (! (current-path!)))]
+                            ; popstate is back/forward event, platform has already set the new location
+                            (.addEventListener js/window "popstate" f)
+                            (fn unmount []
+                              #_(println ::unmount)
+                              (.removeEventListener js/window "popstate" f))))))
+       ; userland route events, because platform pushState and replaceState do not trigger popstate
+       (m/?> (mbx->flow mbx))))))
 
-(tests "nil is discarded"
-  (let [router (from {"/" :slash "/foo" :foo} (constantly "/foo")), it (router #(tap ::notified) #(do))]
-    (with it                   % := ::notified  @it := :foo
-      (route! "/")             % := ::notified  @it := :slash
-      (route! "/doesnt-exist")          ; nil is discarded
-      (route! "/foo")          % := ::notified  @it := :foo)))
+(tests
+  ; careful, only passes from a clean page due to (?)
+  (def !route (m/mbx))
+  (def route> (path> !route))
 
-(tests "a nil in-between same routes still dedupes"
-  (let [router (from {"/" :slash "/foo" :foo} (constantly "/foo")), it (router #(tap ::notified) #(do))]
-    (with it                   % := ::notified  @it := :foo
-      (route! "/")             % := ::notified  @it := :slash
-      (route! "/doesnt-exist")          ; nil is discarded
-      (route! "/")                      ; deduped
-      (route! "/foo")          % := ::notified  @it := :foo)))
+  (def it (route> #(tap ::notify) #(tap ::terminate)))
+  % := ::notify @it := _
+  (replaceState! !route "/")   % := ::notify @it := "/"
+  (pushState! !route "/a")     % := ::notify @it := "/a"
+  (pushState! !route "/a?b")   % := ::notify @it := "/a?b"
+  (pushState! !route "/a?b#c") % := ::notify @it := "/a?b#c"
 
-(tests "first route goes through match-fn"
-  (let [router (from tap), it (router #(do) #(do))]
-    (with it @it := %)))
+  "back/forward"
+  ; the effect is async, wait for notify before sample
+  (.back js/window.history)    % := ::notify @it := "/a?b"
+  (.forward js/window.history) % := ::notify @it := "/a?b#c"
 
-(tests "going back and forward triggers routing"
-  (let [router (from {"/" :slash "/foo" :foo} (constantly "/foo")), it (router #(tap ::notified) #(do))]
-    (with it                       % := ::notified  @it := :foo
-      (route! "/")                 % := ::notified  @it := :slash
-      (route! "/foo")              % := ::notified  @it := :foo
-      (.back js/window.history)    % := ::notified  @it := :slash
-      (.forward js/window.history) % := ::notified  @it := :foo)))
+  ; must sample terminal value to cause m/observe to unmount - due to backpressure
+  (it) % := ::notify @it :throws missionary.Cancelled) ; Mailbox fetch cancelled.
+
+; Demo
+
+(p/def !route)
+(p/def routes {})
+
+(p/defn Link [route label]
+  (let [href (get (clojure.set/map-invert routes) route route)] ; backwards compat
+    (ui/element dom/a {::dom/href href
+                       ::ui/click-event (p/fn [e]
+                                          (pushState! !route href)
+                                          (.preventDefault e))} label)))
+
+(p/defn App []
+  (binding [!route (m/mbx)]
+    (let [route (new (m/relieve {} (path> !route #_js/window)))]
+      (case route
+        "/" (do (dom/h1 "Home") (Link. "/a" "a"))
+        "/a" (do (dom/h1 "A") (Link. "/" "home"))
+        (dom/h1 "404 no matching route: " (pr-str route))))))
+
+(p/defn App2 []
+  (binding [!route (m/mbx)
+            routes {"/" ::home "/b" ::b}] ; optional
+    (let [route (new (m/relieve {} (path> !route #_js/window)))]
+      (case (routes route)
+        ::home (do (dom/h1 "Home") (Link. ::b "b"))
+        ::b (do (dom/h1 "B") (Link. ::home "home"))
+        (dom/h1 "404 no matching route: " (pr-str route))))))
+
+; missing route? doesn't matter, let userland handle it
