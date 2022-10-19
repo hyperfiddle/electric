0a1b9715691b09c6ccf7b18ed58758083dda1748
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-10-19 13:38:47 +0200

    hfql - colors add copyrights notice

--- src/contrib/hsluv.js
+++ src/contrib/hsluv.js
@@ -1,3 +1,23 @@
+// Copyright (c) 2012-2022 Alexei Boronine
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Compiled from https://github.com/hsluv/hsluv-javascript v1.0.0. Feature set
 // is frozen. Also available as an NPM dep, added to source controle to avoid an
 // extra NPM step for demos. TODO consider porting to cljc.
