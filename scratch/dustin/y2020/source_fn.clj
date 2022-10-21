cdb7d10bb58ac6ad8148766c769cc1a10b7ec179
Dustin Getz <dustin.getz@gmail.com>
2022-10-21 15:45:19 -0400

    scratch: cleanup source-fn

--- scratch/dustin/y2020/source_fn.clj
+++ scratch/dustin/y2020/source_fn.clj
@@ -1,33 +1,48 @@
-    (defn source-fn
-      "Returns a string of the source code for the given symbol, if it can
-      find it.  This requires that the symbol resolve to a Var defined in
-      a namespace for which the .clj is in the classpath.  Returns nil if
-      it can't find the source.  For most REPL usage, 'source' is more
-      convenient.
+(ns dustin.y2020.source-fn
+  (:require [hyperfiddle.rcf :refer [tests]])
+  (:import (clojure.lang RT)
+           (java.io InputStreamReader LineNumberReader PushbackReader)))
 
-      Example: (source-fn 'filter)"
-      [x]
-      (when-let [v (resolve x)]
-        (when-let [filepath (:file (meta v))]
-          (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
-            (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
-              (dotimes [_ (dec (:line (meta v)))] (.readLine rdr))
-              (let [text (StringBuilder.)
-                    pbr (proxy [PushbackReader] [rdr]
-                          (read [] (let [i (proxy-super read)]
-                                     (.append text (char i))
-                                     i)))
-                    read-opts (if (.endsWith ^String filepath "cljc") {:read-cond :allow} {})]
-                (if (= :unknown *read-eval*)
-                  (throw (IllegalStateException. "Unable to read source while *read-eval* is :unknown."))
-                  (read read-opts (PushbackReader. pbr)))
-                (str text)))))))
+(defn source-fn
+  "Returns a string of the source code for the given symbol, if it can
+  find it.  This requires that the symbol resolve to a Var defined in
+  a namespace for which the .clj is in the classpath.  Returns nil if
+  it can't find the source.  For most REPL usage, 'source' is more
+  convenient.
 
-    (defmacro source
-      "Prints the source code for the given symbol, if it can find it.
-      This requires that the symbol resolve to a Var defined in a
-      namespace for which the .clj is in the classpath.
+  Example: (source-fn 'filter)"
+  [x]
+  (when-let [v (resolve x)]
+    (when-let [filepath (:file (meta v))]
+      (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
+        (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
+          (dotimes [_ (dec (:line (meta v)))] (.readLine rdr))
+          (let [text (StringBuilder.)
+                pbr (proxy [PushbackReader] [rdr]
+                      (read [] (let [i (proxy-super read)]
+                                 (.append text (char i))
+                                 i)))
+                read-opts (if (.endsWith ^String filepath "cljc") {:read-cond :allow} {})]
+            (if (= :unknown *read-eval*)
+              (throw (IllegalStateException. "Unable to read source while *read-eval* is :unknown."))
+              (read read-opts (PushbackReader. pbr)))
+            (str text)))))))
 
-      Example: (source filter)"
-      [n]
-      `(println (or (source-fn '~n) (str "Source not found"))))
+(defmacro source
+  "Prints the source code for the given symbol, if it can find it.
+  This requires that the symbol resolve to a Var defined in a
+  namespace for which the .clj is in the classpath.
+
+  Example: (source filter)"
+  [n]
+  `(println (or (source-fn '~n) (str "Source not found"))))
+
+(tests
+  (source-fn `first)
+  := "(def
+ ^{:arglists '([coll])
+   :doc \"Returns the first item in the collection. Calls seq on its
+    argument. If coll is nil, returns nil.\"
+   :added \"1.0\"
+   :static true}
+ first (fn ^:static first [coll] (. clojure.lang.RT (first coll))))")
