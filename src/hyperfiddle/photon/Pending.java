614391f254d4047c1bceeb8b9c1beae9f8e0c81c
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-11-05 17:17:38 +0100

    perf - faster Pending instantiation
    Pending extends Throwable, which fills in a stacktrace on instantiation by
    default. Computing the stack trace is an expensive operation. We don’t need
    stacktraces in Pending. This fix bypasses the stacktrace building.

--- src/hyperfiddle/photon/Pending.java
+++ src/hyperfiddle/photon/Pending.java
@@ -1,5 +1,9 @@
 package hyperfiddle.photon;
 public class Pending extends Throwable {
+    public Pending() {
+        super(null, null, false, false);
+    }
+
     public boolean equals(Object o){
         return (o instanceof Pending);
     }
