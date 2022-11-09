7470f93d0e6334d6d7f928c264fa228630071b48
xificurC <xificurC@gmail.com>
2022-11-09 09:50:27 +0100

    fix browser test runner (for me)

--- ci/run_tests_browser.sh
+++ ci/run_tests_browser.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/sh -x
 
 echo "Running Browser tests"
-./node_modules/.bin/shadow-cljs -A:test compile :browser-test --force-spawn
+./node_modules/.bin/shadow-cljs -A:dev:test compile :browser-test --force-spawn
 ./node_modules/.bin/karma start --single-run $@ # --browsers Chrome
