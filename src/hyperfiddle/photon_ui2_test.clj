9c2876ba86e3313ebee04d644a8f8ba445d36814
xificurC <xificurC@gmail.com>
2022-11-23 14:44:13 +0100

    fix ui/input tests

--- src/hyperfiddle/photon_ui2_test.clj
+++
@@ -1,4 +0,0 @@
-(ns hyperfiddle.photon-ui2-test
-  (:require [hyperfiddle.photon :as p]
-            [hyperfiddle.photon-dom :as dom]))
-(defmacro run [& body] `(with (p/run (binding [dom/node (dom/by-id "root")] ~@body))))
--- src/hyperfiddle/photon_ui2_test.cljs
+++ src/hyperfiddle/photon_ui2_test.cljs
@@ -3,34 +3,40 @@
    [hyperfiddle.photon :as p]
    [hyperfiddle.photon-dom :as dom]
    [hyperfiddle.photon-ui2 :as ui]
-   [hyperfiddle.rcf :as rcf :refer [% tap tests]]
-   [missionary.core :as m])
-  (:require-macros hyperfiddle.photon-ui2-test))
+   [hyperfiddle.rcf :as rcf :refer [% tap tests with]]))
 
 (tests "ui/input accepts literal props map"
-  (run (ui/input "controlled-value" {:style {:width "100px"}}
-         (tap (-> dom/node .-style .-cssText))))
-  % := "width: 100px;")
+  (with (p/run (binding [dom/node (dom/by-id "root")]
+                 (ui/input "controlled-value" {:style {:width "100px"}}
+                   (tap (-> dom/node .-style .-cssText)))))
+    % := "width: 100px;"))
 
-;; TODO figure this out
-#_
+(defn focus [elem]
+  (.dispatchEvent elem (js/FocusEvent. "focus"))
+  (.focus elem #js {"focusVisible" true}))
+
+(defn blur [elem]
+  (.dispatchEvent elem (js/FocusEvent. "blur"))
+  (.blur elem))
+
+(defn focused? [elem] (= (.-activeElement js/document) elem))
+
+(defn swap-input! [elem f & args]
+  (let [v (apply f (.-value elem) args)]
+    (set! (.-value elem) v)
+    (.dispatchEvent elem (js/InputEvent. "input"))
+    v))
+
 (tests "ui/input blur reverts to original value"
   (def in (atom nil))
   (def discard (p/run (binding [dom/node (dom/by-id "root")]
                         (tap (ui/input "controlled-value"
                                (reset! in dom/node))))))
-  ((m/reduce (fn [_ _] (tap :focused)) nil (m/observe (fn [!]
-                                                        (.addEventListener @in "focus" !)
-                                                        #(.removeEventListener @in "focus" !))))
-   identity identity)
   % := "controlled-value"
-  (.focus @in)
-  % := :focused
-  ;; this fails, probably async?
-  ;; (.-activeElement js/window) := @in
-  ;; (set! (.-value @in) "new-value")
-  ;; or (.dispatchEvent @in (js/KeyboardEvent. #js {:key "x"}))
-  ;; % := "new-value"
-  ;; (.blur @in)
-  ;; % := "controlled-value"
+  (focus @in)
+  (focused? @in) := true
+  (swap-input! @in (constantly "new-value"))
+  % := "new-value"
+  (blur @in)
+  % := "controlled-value"
   (discard))
