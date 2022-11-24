c76362a3250f49f8db8f38dd5a4899ab30a300f9
xificurC <xificurC@gmail.com>
2022-11-24 21:16:18 +0100

    photon-ui2 test cleanup

--- src/hyperfiddle/photon_ui2_test.cljs
+++ src/hyperfiddle/photon_ui2_test.cljs
@@ -13,12 +13,6 @@
 
 (ensure-root!)
 
-(tests "ui/input accepts literal props map"
-  (with (p/run (binding [dom/node (dom/by-id "root")]
-                 (ui/input "controlled-value" {:style {:width "100px"}}
-                   (tap (-> dom/node .-style .-cssText)))))
-    % := "width: 100px;"))
-
 (defn focus [elem]
   (.dispatchEvent elem (js/FocusEvent. "focus"))
   (.focus elem #js {"focusVisible" true}))
@@ -29,11 +23,17 @@
 
 (defn focused? [elem] (= (.-activeElement js/document) elem))
 
-(defn swap-input! [elem f & args]
+(defn swap-value! [elem f & args]
   (let [v (apply f (.-value elem) args)]
     (set! (.-value elem) v)
     (.dispatchEvent elem (js/InputEvent. "input"))
     v))
+
+(tests "ui/input accepts literal props map"
+  (with (p/run (binding [dom/node (dom/by-id "root")]
+                 (ui/input "controlled-value" {:style {:width "100px"}}
+                   (tap (-> dom/node .-style .-cssText)))))
+    % := "width: 100px;"))
 
 (tests "ui/input blur reverts to original value"
   (def in (atom nil))
@@ -43,7 +43,7 @@
   % := "controlled-value"
   (focus @in)
   (focused? @in) := true
-  (swap-input! @in (constantly "new-value"))
+  (swap-value! @in (constantly "new-value"))
   % := "new-value"
   (blur @in)
   % := "controlled-value"
