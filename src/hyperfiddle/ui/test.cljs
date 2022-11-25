8a9de7a320778bce59d5dfaf4abee771adbcde4d
xificurC <xificurC@gmail.com>
2022-11-25 17:24:37 +0100

    factor out helpers for next test ns

---
+++ src/hyperfiddle/ui/test.cljs
@@ -0,0 +1,31 @@
+(ns hyperfiddle.ui.test
+  (:require [hyperfiddle.photon-dom :as dom]))
+
+(defn ensure-root! []
+  (when-not (dom/by-id "root")
+    (let [root (.createElement js/document "div")]
+      (set! (.-id root) "root")
+      (.appendChild (.-body js/document) root))))
+
+(ensure-root!)
+
+(defn focus [elem]
+  (.dispatchEvent elem (js/FocusEvent. "focus"))
+  (.focus elem #js {"focusVisible" true}))
+
+(defn blur [elem]
+  (.dispatchEvent elem (js/FocusEvent. "blur"))
+  (.blur elem))
+
+(defn focused? [elem] (= (.-activeElement js/document) elem))
+
+(defn swap-value! [elem f & args]
+  (let [v (apply f (.-value elem) args)]
+    (set! (.-value elem) v)
+    (.dispatchEvent elem (js/InputEvent. "input"))
+    (.dispatchEvent elem (js/InputEvent. "change"))
+    v))
+
+(defn toggle! [elem]
+  (set! (.-checked elem) (not (.-checked elem)))
+  (.dispatchEvent elem (js/Event. "change")))
