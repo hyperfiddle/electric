db292fe6f03cde0fcd7b913a263906871fe3970d
Dustin Getz <dustin.getz@gmail.com>
2022-12-14 15:09:53 -0500

    toxiproxy - moved to src-dev

--- src-dev/toxiproxy.sh
+++ src-dev/toxiproxy.sh
@@ -5,6 +5,7 @@
 
 toxiproxy-server & \
 toxiproxy-cli create --listen localhost:$1 --upstream localhost:$2 hf_toxic_proxy && \
-toxiproxy-cli toxic add -t latency -a latency=150 hf_toxic_proxy
+toxiproxy-cli toxic add -t latency -a latency=$3 hf_toxic_proxy
 
-# killall toxyproxy-server
+# ./toxiproxy.sh 8083 8080 500
+# killall toxiproxy-server
