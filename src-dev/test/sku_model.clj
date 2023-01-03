b032de0948158643c0b5827b50a195a1b53e8288
Dustin Getz <dustin.getz@gmail.com>
2023-01-03 14:01:38 -0500

    test models: stubs for sku model and social-news model from cognitect

---
+++ src-dev/test/sku_model.clj
@@ -0,0 +1,3 @@
+(ns test.sku-model)
+
+; https://github.com/Datomic/day-of-datomic/blob/master/tutorial/filters.clj
---
+++ src-dev/test/social_news.clj
@@ -0,0 +1,3 @@
+(ns test.social-news)
+
+; https://gist.github.com/stuarthalloway/2948756
