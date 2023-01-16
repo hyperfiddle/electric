a05e735ae6f7e54657863899feca38008461d514
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-01-16 18:16:00 +0100

    Integrated router - simplify entrypoint

--- src/hyperfiddle/router.clj
+++ src/hyperfiddle/router.clj
@@ -1,4 +1,15 @@
 (ns hyperfiddle.router
-  (:require [missionary.core :as m]))
+  (:require [missionary.core :as m]
+            [hyperfiddle.api :as hf]
+            [contrib.ednish]))
 
 (defmacro path [!path] `(new (m/relieve {} (hyperfiddle.router/path> ~!path))))
+
+(defmacro html5-router [Current-route & body]
+  `(let [current-route# ~Current-route]
+     (hf/router
+       (p/fn [!path#] (new current-route# (contrib.ednish/decode-path (path !path#) hf/read-edn-str)))
+       hyperfiddle.router/html5-navigate!
+       #(.back js/window.history)
+       hyperfiddle.router/html5-replace-state!
+       ~@body)))
