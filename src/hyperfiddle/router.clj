bd0e7a925197f6b7fa100f2aa9edae67b335f405
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-01-19 15:42:45 +0100

    Drop legacy router

--- src/hyperfiddle/router.clj
+++
@@ -1,7 +0,0 @@
-(ns hyperfiddle.router
-  (:require [missionary.core :as m]
-            [hyperfiddle.api :as hf]
-            [contrib.ednish]))
-
-(defmacro path [!path] `(new (m/relieve {} (hyperfiddle.router/path> ~!path))))
-
