b4ab8dd805f69f3273932609c2111888b68b29ba
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-01-23 17:50:23 +0100

    Fix uuid not supported in cljs ednish
    Also add `contrib.ednish` and `contrib.sexpr-router` to the test suite

--- ci/run_tests_jvm.sh
+++ ci/run_tests_jvm.sh
@@ -9,4 +9,7 @@ clojure -X:test \
       \"hyperfiddle.zero\"                   \
       \"hyperfiddle.missionary-test\"        \
       \"hyperfiddle.logger-test\"            \
-      \"hyperfiddle.core-async-test\"]"
+      \"hyperfiddle.core-async-test\"        \
+      \"contrib.ednish\"                     \
+      \"contrib.sexpr-router\"               \
+     ]"
