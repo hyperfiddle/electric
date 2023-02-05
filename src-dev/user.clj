5df098ae59cf27298415905cdf0b23a51a4c11c0
Dustin Getz <dustin.getz@gmail.com>
2023-02-05 15:56:33 -0500

    pass over entrypoint, simplify and reduce LOC

--- src-dev/user.clj
+++ src-dev/user.clj
@@ -1,61 +1,34 @@
-(ns user ; Must be ".clj" file, Clojure will not auto-run user.cljc
+(ns user ; Must be ".clj" file, Clojure doesn't auto-load user.cljc
   "Start a REPL with `clj -A:dev`, or jack in with :dev alias."
   (:refer-clojure :exclude [compile])
-
-  ; For rapid REPL startup, put absolute minimum of requires here: REPL conveniences only,
-  ; which includes clojure reader extensions listed in data_readers.cljc.
+  ; For fastest REPL startup, no heavy deps here, REPL conveniences only
+  ; (Clojure has to compile all this stuff on startup)
   (:require [missionary.core :as m]
             hyperfiddle.rcf))
 
-; Userland photon application code will be lazy loaded by the shadow build `(main)`
-; due to :require-macros in all Photon source files.
+; Userland photon code is lazy loaded by the shadow build due to usage of :require-macros
+; in all Photon source files.
 ; WARNING: make sure your REPL and shadow-cljs are sharing the same JVM!
 
 (comment
-  "Photon Clojure REPL entrypoint"
-  (main)
-
-  "turn on RCF (after all transitive dependencies have loaded)"
-  (hyperfiddle.rcf/enable!)
-
-  (shadow.cljs.devtools.api/compile :editor)
-
-  "Switch entrypoints"
-  (user/browser-main! `user-main/Main) ; see user.cljs
-
-  "ClojureScript REPL entrypoint"
-  ; shadow server exports an repl, connect a second REPL instance to it (DO NOT REUSE JVM REPL it will fail weirdly)
-  (shadow.cljs.devtools.api/repl :dev)
+  (main) ; Photon Clojure(JVM) REPL entrypoint
+  (hyperfiddle.rcf/enable!) ; turn on RCF after all transitive deps have loaded
+  (shadow.cljs.devtools.api/repl :dev) ; shadow server hosts the cljs repl
+  ; connect a second REPL instance to it
+  ; (DO NOT REUSE JVM REPL it will fail weirdly)
   (type 1))
 
-(defmacro get-main [default]
-  (list 'quote (or (some-> (System/getenv "HF_DEMO") symbol) default)))
-
-(def cljs-eval (delay @(requiring-resolve 'shadow.cljs.devtools.api/cljs-eval)))
 (def shadow-start! (delay @(requiring-resolve 'shadow.cljs.devtools.server/start!)))
 (def shadow-watch (delay @(requiring-resolve 'shadow.cljs.devtools.api/watch)))
 (def shadow-compile (delay @(requiring-resolve 'shadow.cljs.devtools.api/compile)))
 (def shadow-release (delay @(requiring-resolve 'shadow.cljs.devtools.api/release)))
-(def start-server! (delay @(requiring-resolve 'hyperfiddle.photon-jetty-server/start-server!)))
+(def start-photon-server! (delay @(requiring-resolve 'hyperfiddle.photon-jetty-server/start-server!)))
 (def rcf-enable! (delay @(requiring-resolve 'hyperfiddle.rcf/enable!)))
 
-(defn browser-main! "hot switch reactor entrypoint from CLJ REPL" [photon-main-sym]
-  ; Save the user the trouble of getting a CLJS repl to switch photon entrypoints
-  (@cljs-eval :dev (str `(println ::loading (quote ~photon-main-sym))) {})
-  (@cljs-eval :dev (str `(browser-main! (quote ~photon-main-sym))) {})
-  (fn dispose [] (@cljs-eval :dev `(user/stop!) {})))
+(defn rcf-shadow-hook {:shadow.build/stages #{:compile-prepare :compile-finish}}
+  [build-state & args] build-state)
 
-(comment (@(requiring-resolve 'shadow.cljs.devtools.api/cljs-eval) :dev (str "(println ::x)") {}))
-
-(defn serve! "Start Photon app server" []
-  (let [host "0.0.0.0"]
-    (def server (@start-server! {:host host, :port 8080, :resources-path "resources/public"}))
-    (println (str "\n👉 App server available at http://" host ":" (-> server (.getConnectors) first (.getPort))
-             "\n"))))
-
-(defn rcf-shadow-hook {:shadow.build/stages #{:compile-prepare :compile-finish}} [build-state & args] build-state)
-
-(defn install-shadow-hook! []
+(defn install-rcf-shadow-hook []
   (alter-var-root #'rcf-shadow-hook
     (constantly (fn [build-state & args]
                   ;; NOTE this won’t prevent RCF tests to run during :require-macros phase
@@ -64,17 +37,18 @@
                     :compile-finish (@rcf-enable!))
                   build-state))))
 
-(defn main "CLJ main" [& args]
+(def photon-server-config {:host "0.0.0.0", :port 8080, :resources-path "resources/public"})
+
+(defn main [& args]
   (println "Starting Photon compiler and server...")
-  "build and serve clojurescript assets"
-  (@shadow-start!)                                          ; serves index.html as well
-  (@rcf-enable! false) ; don't run cljs tests on compile - in case user enabled at the REPL
+  (@shadow-start!) ; serves index.html as well
+  (@rcf-enable! false) ; don't run cljs tests on compile (user may have enabled it at the REPL already)
   (@shadow-watch :dev) ; depends on shadow server
   ; todo report clearly if shadow build failed, i.e. due to yarn not being run
-  (serve!)
+  (def server (@start-photon-server! photon-server-config))
   (comment (.stop server))
 
-  "Datomic Cloud (requires :scratch alias)"
+  "Datomic Cloud (optional, requires :scratch alias)"
   (require '[contrib.datomic-m :as d])
   (when (not-empty (eval '(d/detect-datomic-products)))
     #_(contrib.datomic-m/install-datomic-onprem)
@@ -84,30 +58,19 @@
     (def datomic-client (eval '(d/client datomic-config)))
     (def datomic-conn (m/? (eval '(d/connect datomic-client {:db-name "mbrainz-subset"}))))
 
-    ; install test globals, which can be different
+    ; install test globals, which are different
     (require 'test)
     (eval '(test/install-test-state)))
 
   ; enable RCF after Datomic is loaded – to resolve circular dependency
-  (install-shadow-hook!)
+  (install-rcf-shadow-hook)
   (@rcf-enable!))
 
-(defn compile []
-  ; optimized artifact but with debug information available to find problems
-  (@shadow-compile :dev))
+;(defn compile [] (@shadow-compile :dev))
+;(defn release [] (@shadow-release :dev {:debug true}))
 
-(defn release []
-  ; optimized artifact but with debug information available to find problems
-  (@shadow-release :app {:debug true}))
+;(when (get (System/getenv) "REPLIT_ENVIRONMENT")
+;  (compile) (@start-photon-server! photon-server-config))
 
-(defn boot! []
-  (compile)
-  (serve!))
-
-(when (get (System/getenv) "REPLIT_ENVIRONMENT")
-  (boot!))
-
-(when (contains? (System/getenv) "HYPERFIDDLE_DEV")
-  "auto boot"
-  (main)) ; this blocks the repl until build is ready. alternatively can run in a future?
-
+(when (contains? (System/getenv) "HYPERFIDDLE_AUTO_BOOT")
+  (main)) ; blocks repl until build is ready, should we run it in a future?
