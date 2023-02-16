c3111547974391a5deffff3dbe1d27d066006416
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-02-16 14:57:04 +0100

    Fix electric demo lifecycle

--- src-docs/user/electric/electric_lifecycle.clj
+++ src-docs/user/electric/electric_lifecycle.clj
@@ -1,12 +1,12 @@
-(ns user.photon.photon-lifecycle
-  (:require [hyperfiddle.electric :as p]
+(ns user.electric.electric-lifecycle
+  (:require [hyperfiddle.electric :as e]
             [hyperfiddle.rcf :refer [tests tap % with]]
             [missionary.core :as m]))
 
 
 (hyperfiddle.rcf/enable!)
 
-; Missionary flows, and transitively Photon flows, have an object lifecycle (mount/unmount in React terms).
+; Missionary flows, and transitively Electric Clojure flows, have an object lifecycle (mount/unmount in React terms).
 ; Reactive computations have a "boot" phase (the expensive initial compute). Once booted, flows are incrementally
 ; maintained (a small change to an input results in a small adjustment to the output). Eventually, the program will
 ; terminate gracefully, terminate with error (crash), or be cancelled from the outside.
@@ -28,17 +28,17 @@
 
 (tests
   (def !x (atom 0))
-  (with (p/run (tap (new (MyWatch !x))))                      ; Functions that return flows are "DAG constructors"
+  (with (e/run (tap (new (MyWatch !x))))                      ; Functions that return flows are "DAG constructors"
     % := 0
     (swap! !x inc)
     % := 1))
 
 ; `MyWatch` is a function that constructs a missionary flow (DAG) and returns it as a value.
-; In Photon, use (new) to join such a flow value into the Photon execution context.
+; In Electric Clojure, use (new) to join such a flow value into the Electric Clojure execution context.
 ; This operation of flattening a flow-of-flows into the main flow is the same as "await" or "flatmap" aka "monadic join".
 
 
-; m/observe is how you hook the Photon object lifecycle:
+; m/observe is how you hook the Electric Clojure object lifecycle:
 
 (defn ConstantObject "Returns a recipe for a constant signal that logs its lifecycle to provided callback"
   [x trace!]
@@ -53,11 +53,11 @@
 (tests "object lifecycle"
   (def !x (atom 0))
   (def dispose
-    (p/run
+    (e/run
       (tap
-        (let [x (p/watch !x)]
-          (if (even? x)
-            (new (ConstantObject x tap)))))))                       ; Photon will unmount/destruct the flow when the if switches back
+        (let [x (e/watch !x)]
+          (when (even? x)
+            (new (ConstantObject x tap))))))) ; Electric Clojure will unmount/destruct the flow when the if switches back
   % := ::mount
   % := 0
   (swap! !x inc)
