bd2294fbbbb2e8ff1396e851befa3d6a6c24efe8
Dustin Getz <dustin.getz@gmail.com>
2023-02-19 14:49:35 -0500

    build.sh

---
+++ src-build/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+clojure -T:build clean && rm -rf ./resources/public/js
+#clj -A:dev -X user/release -- distribute sources, lib consumer will build
+HYPERFIDDLE_ELECTRIC_BUILD=`git describe --tags --long`
+clojure -T:build jar :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
+clojure -T:build install :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
