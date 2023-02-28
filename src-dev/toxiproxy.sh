e65045f4c8950da98f436391997903385e6bea2e
Dustin Getz <dustin.getz@gmail.com>
2023-02-28 10:58:53 -0500

    toxiproxy: improve instructions and shorten default latency
    HTML initial page load involves many round trips during asset/script loading which explode this time out unreasonably

--- src-dev/toxiproxy.sh
+++ src-dev/toxiproxy.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+### INSTALLATION
+
 # https://github.com/Shopify/toxiproxy
 # brew tap shopify/shopify
 # brew install toxiproxy
+
+### RUNNING
+
+# ./toxiproxy.sh 8083 8080 200
+# toxiproxy-cli list
+# killall toxiproxy-server
+# toxiproxy-cli toxic help
+# toxiproxy-cli toxic update --toxicName hf_latency_toxic --attribute latency=500 hf_dev_proxy
+
 
 log_level=fatal # even 'error' is too spammy
 
@@ -15,10 +26,3 @@ done
 echo " server up"
 toxiproxy-cli create --listen 0.0.0.0:$1 --upstream localhost:$2 hf_dev_proxy && \
 toxiproxy-cli toxic add --toxicName hf_latency_toxic --type latency --attribute latency=$3 hf_dev_proxy
-
-# ./toxiproxy.sh 8083 8080 500
-# toxiproxy-cli list
-# killall toxiproxy-server
-
-# toxiproxy-cli toxic help
-# toxiproxy-cli toxic update --toxicName hf_latency_toxic --attribute latency=500 hf_dev_proxy
