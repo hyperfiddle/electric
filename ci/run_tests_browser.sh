87f5898613b80b4c3b67ad11a72a48ee35befbf6
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-03-10 17:02:33 +0100

    Use clojure instead of clj in github actions scripts
    `clj` requires rlwrap, `clojure` doesn’t

--- ci/run_tests_browser.sh
+++ ci/run_tests_browser.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
 echo "Running Browser tests"
-clj -M:dev:test:shadow-cljs compile :browser-test --force-spawn
+clojure -M:dev:test:shadow-cljs compile :browser-test --force-spawn
 ./node_modules/.bin/karma start --single-run $@ # --browsers Chrome
