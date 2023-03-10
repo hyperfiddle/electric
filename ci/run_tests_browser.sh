b5c3a24cdeb9505b6c6a61a887fc8b454506ce84
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-03-10 15:41:21 +0100

    Drop the need for npm shadow-cljs executable
    Use `clj -M:shadow-cljs compile dev` instead

--- ci/run_tests_browser.sh
+++ ci/run_tests_browser.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
 echo "Running Browser tests"
-./node_modules/.bin/shadow-cljs -A:dev:test compile :browser-test --force-spawn
+clj -M:dev:test:shadow-cljs compile :browser-test --force-spawn
 ./node_modules/.bin/karma start --single-run $@ # --browsers Chrome
