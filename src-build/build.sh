82725c736f2ceb12a23d37e6883f99f6d79edbbb
Dustin Getz <dustin.getz@gmail.com>
2023-04-08 13:30:47 -0400

    build script polish

--- src-build/build.sh
+++ src-build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-clojure -T:build clean
+clojure -T:build clean && rm -rf ./resources/public/js
 #clj -A:dev -X user/release -- distribute sources, lib consumer will build
-HYPERFIDDLE_ELECTRIC_BUILD=`git describe --tags --long`
-clojure -X:build jar :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
-clojure -X:build install :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
+HYPERFIDDLE_ELECTRIC_BUILD=`git describe --tags --long --always --dirty`
+clojure -T:build jar :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
+clojure -T:build install :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
