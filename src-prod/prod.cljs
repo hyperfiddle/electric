81c85642886a2d5067ac22e36bd3e21052ff7d70
Dustin Getz <dustin.getz@gmail.com>
2023-04-11 09:39:11 -0400

    boot: move reload-when-stale logic into library entrypoint

--- src-prod/prod.cljs
+++ src-prod/prod.cljs
@@ -7,8 +7,4 @@
 (defn start! []
   (electric-main
     #(js/console.log "Reactor success:" %)
-    (fn [error]
-      (case (:hyperfiddle.electric/type (ex-data error))
-        :hyperfiddle.electric-client/stale-client (do (js/console.log "Server and client version mismatch. Refreshing page.")
-                                                      (.reload (.-location js/window)))
-        (js/console.error "Reactor failure:" error)))))
+    #(js/console.error "Reactor failure:" %)))
