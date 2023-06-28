4dce19e83967fdd79c9ce433a3c8e59c1060692e
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-06-28 16:08:59 +0200

    Implement WS heartbeat at the message level for HTTPKit compat.
    Server used to send PING frames to client, which is more correct.
    We want Jetty and HTTPKit compat. Jetty supports PING/PONG, not HTTPKit.
    
    Server is now passive, and keeps the connection alive for some times after the
    last received message. Client periodically sends the message "HEARTBEAT". Server
    detects "HEARTBEAT" and discard it.

--- src/hyperfiddle/electric_jetty_adapter.clj
+++ src/hyperfiddle/electric_jetty_adapter.clj
@@ -9,19 +9,6 @@
            [java.nio ByteBuffer]
            [org.eclipse.jetty.websocket.api Session WebSocketAdapter]))
 
-(defn make-heartbeat
-  "Ping the client to prevent connection timeout and detect unexpected disconnects.
-  Close the connection when the client is not reachable anymore."
-  [^Session s pong-mailbox]
-  ;; Ping wisely https://developer.android.com/training/connectivity/network-access-optimization#RadioStateMachine
-  (m/sp
-    (loop []
-      (m/? (m/sleep 15000))
-      (.sendPing (.getRemote s) (ByteBuffer/allocate 0))
-      (when-not (m/? (m/timeout pong-mailbox 20000))
-        (throw (ex-info "Websocket pong timeout." {})))
-      (recur))))
-
 (defn failure [^WebSocketAdapter ws ^Throwable e]
   (if (instance? Cancelled e)
     (log/debug "Websocket handler completed gracefully.")
@@ -40,40 +27,51 @@
       (catch Throwable e (f e)))
     #()))
 
+(defn timeout
+  "Throw if `mailbox` haven't got any message after given `time` ms"
+  [mailbox time]
+  (m/sp
+    (loop []
+      (when (= :timeout (m/? (m/timeout mailbox time :timeout)))
+        (throw (ex-info "No message received after specified time" {:time time})))
+      (recur))))
+
 (defn electric-ws-adapter
   "Start and manage an Electric server process hooked onto a websocket."
   [handler-f]
-  (let [state        (object-array 2)
-        on-message-slot (int 0)
-        on-close-slot   (int 1)
-        pong-mailbox (m/mbx)]
+  (let [state             (object-array 2)
+        on-message-slot   (int 0)
+        on-close-slot     (int 1)
+        keepalive-mailbox (m/mbx)]
     {:on-connect (fn on-connect [^WebSocketAdapter ws]
                    (log/debug "WS connect" (jetty/req-of ws))
-                   (let [session (.getSession ws)]
-                     (.setMaxTextMessageSize (.getPolicy session) (* 100 1024 1024))  ; Allow large value paylods, temporary.
-                     (aset state on-close-slot
-                       ((m/join {} (make-heartbeat session pong-mailbox)
-                          (handler-f (partial write-msg ws)
-                            (r/subject-at state on-message-slot)))
-                        {} (partial failure ws)))))  ; Start Electric process
+                   (.setMaxTextMessageSize (.getPolicy (.getSession ws)) (* 100 1024 1024))  ; Allow large value payloads, temporary.
+                   (aset state on-close-slot
+                     ((m/join {} (timeout keepalive-mailbox 90000)
+                        (handler-f (partial write-msg ws)
+                          (r/subject-at state on-message-slot)))
+                      {} (partial failure ws))))  ; Start Electric process
      :on-close   (fn on-close [ws status-code reason]
                    (let [status {:status status-code, :reason reason}]
                      (case (long status-code) ; https://www.rfc-editor.org/rfc/rfc6455.html#section-7.4.1
                        1000 (log/debug "Client disconnected gracefully" status)
                        1001 (log/debug "Client navigated away" status)
-                       ;; 1005 is the default close code set by Chrome an FF unless specified.
+                       ;; 1005 is the default close code set by Chrome and FF unless specified.
                        1005 (log/debug "Client disconnected for an unknown reason (browser default close code)" status)
                        (log/debug "Client disconnected for an unexpected reason." status))
                      ((aget state on-close-slot))))
      :on-error   (fn on-error [ws err]
                    (log/error err "Websocket error"))
-     :on-ping    (fn on-ping [ws bytebuffer]) ; Ignore client ping, no use case.
-     :on-pong    (fn on-pong [ws bytebuffer]
-                   (log/trace "pong")
-                   (pong-mailbox bytebuffer))
+     :on-ping    (fn on-ping [^WebSocketAdapter ws bytebuffer] ; Send pong and keep connection alive.
+                   (.sendPong (.getRemote (.getSession ws)) bytebuffer)
+                   (keepalive-mailbox nil))
+     :on-pong    (fn on-pong [ws bytebuffer] ; ignore pong, no use case
+                   (log/trace "pong"))
      :on-text    (fn on-text [^WebSocketAdapter ws text]
                    (log/trace "text received" text)
-                   ((aget state on-message-slot) text))
+                   (keepalive-mailbox nil)
+                   (when-not (= "HEARTBEAT" text)
+                     ((aget state on-message-slot) text)))
      :on-bytes   (fn [^WebSocketAdapter ws ^bytes bytes offset length]
                    (log/trace "bytes received" {:length length})
                    ((aget state on-message-slot) (ByteBuffer/wrap bytes offset length)))}))
