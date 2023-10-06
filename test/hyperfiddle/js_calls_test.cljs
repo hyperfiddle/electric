hyperfiddle/electric

commit 1f5edb5ab1c8736d9cfefd091a541d924118f200

Optimize call js globals with the correct `this` context
Previous impl was emitting an IIFE the Google Closure Compiler could not inline.
This change moves the `this` binding from compile time to runtime. The cost is
lower, because the emitted code is smaller and the `this` binding happens on
program initialization, before electric runs.

Example of calling a js global with the wrong `this` context:
`(apply js/alert ["hello"])`
would translate to alert.call(null, ["hello"]), whereas the correct form should
be `alert.call(window, ["hello"])`

src/hyperfiddle/electric/impl/compiler.clj
@@ -236,9 +236,19 @@
        (merge {:file file} debug-info)
        (or debug-info {})))))
 
+(defn bound-js-fn
+  "Given a js global (e.g js/alert or js/console.log), ensures that it is called
+  under the correct `this` context, if it resolves to a function."
+  [sym]
+  (let [fields (clojure.string/split (name sym) #"\.")]
+    `(r/bound-js-fn ~(symbol "js" (if (seq (rest fields))
+                                    (clojure.string/join (interpose '. (butlast fields)))
+                                    "window"))
+       ~sym)))
+
 (defn analyze-global [env sym]
   (case (namespace sym)
-    "js" (ir/eval sym)
+    "js" (ir/eval (bound-js-fn sym))
     (let [sym (if-some [var (env/resolve-var env sym)]
                 (env/var-name var)
                 (case (env/peer-language env)
@@ -634,38 +644,32 @@
           (analyze-sexpr env (with-meta `(new ~(symbol (namespace op) (subs n 0 e)) ~@args) (meta form)))
           (if (contains? (:locals env) op)
             (analyze-apply env form)
-            (if (= "js" (namespace op))
-              ;; calls like js/alert expect this=Window
-              ;; - the thunk forces cljs compilation where this is true
-              ;; - the `let` over args allows electric code (e.g. calling `new`)
-              (let [gs (repeatedly (count form) gensym)]
-                (recur env `(let [[~@gs] [~@form]] (#(~@gs)))))
-              (if-some [sym (env/resolve-runtime env op)]
-                (case sym
-                  (clojure.core/unquote-splicing cljs.core/unquote-splicing) (toggle env (first args) {::dbg/meta (meta form) ::dbg/type :toggle})
-                  (analyze-apply env form))
-                (if-some [v (env/resolve-var env op)]
-                  (case (env/var-name v)
-                    (clojure.core/binding cljs.core/binding)
-                    (analyze-binding env (first args) analyze-sexpr (cons `do (next args)))
+            (if-some [sym (env/resolve-runtime env op)]
+              (case sym
+                (clojure.core/unquote-splicing cljs.core/unquote-splicing) (toggle env (first args) {::dbg/meta (meta form) ::dbg/type :toggle})
+                (analyze-apply env form))
+              (if-some [v (env/resolve-var env op)]
+                (case (env/var-name v)
+                  (clojure.core/binding cljs.core/binding)
+                  (analyze-binding env (first args) analyze-sexpr (cons `do (next args)))
 
-                    (if (env/is-node v)
-                      (analyze-apply env form)
-                      (cond (instance? CljVar v) ; "manual" macroexpansion: call the var as a function, passing it &form and the appropriate &env
-                            (analyze-form env
-                              (binding [*env* env
-                                        *ns* (find-ns (if (:js-globals env) (:name (:ns env)) (:ns env)))]
-                                (apply (env/get-var v) form (if (:js-globals env) env (:locals env)) args)))
+                  (if (env/is-node v)
+                    (analyze-apply env form)
+                    (cond (instance? CljVar v) ; "manual" macroexpansion: call the var as a function, passing it &form and the appropriate &env
+                          (analyze-form env
+                            (binding [*env* env
+                                      *ns* (find-ns (if (:js-globals env) (:name (:ns env)) (:ns env)))]
+                              (apply (env/get-var v) form (if (:js-globals env) env (:locals env)) args)))
 
-                            (instance? CljsVar v) ;; TODO GG: is this case possible? A cljs macro var without a corresponding clj macro var.
-                            (throw (ex-info "Failed to resolve macro expander" {:name (env/var-name v)}))
+                          (instance? CljsVar v) ;; TODO GG: is this case possible? A cljs macro var without a corresponding clj macro var.
+                          (throw (ex-info "Failed to resolve macro expander" {:name (env/var-name v)}))
 
-                            :else (throw (ex-info "Invalid call" {:form form})))))
-                  (let [desugared-form (desugar-host env form)]
-                    (if (= form desugared-form)
-                      ;; Nothing got desugared, this is not host interop, meaning `op` var wasn't found.
-                      (analyze-apply env form) ; pass through
-                      (analyze-form env desugared-form)))))))))
+                          :else (throw (ex-info "Invalid call" {:form form})))))
+                (let [desugared-form (desugar-host env form)]
+                  (if (= form desugared-form)
+                    ;; Nothing got desugared, this is not host interop, meaning `op` var wasn't found.
+                    (analyze-apply env form) ; pass through
+                    (analyze-form env desugared-form))))))))
       (analyze-apply env form))))
 
 (defn analyze-map [env form]

src/hyperfiddle/electric/impl/runtime.cljc
@@ -105,6 +105,12 @@
   (partial m/latest
     (fn [x y] (if (instance? Failure y) y x))))
 
+#?(:cljs
+   (defn bound-js-fn [this ^js obj]
+     (if (instance? js/Function obj)
+       (.bind obj this)
+       obj)))
+
 (defn pure [x] (m/cp x))
 
 (def empty-event

commit c900c45ce3ff48c7c9b6a5426f4a18003d0653fe

Fix js functions equality when accessed from global scope
Commit 1f5edb5a broke equality between global js fns and classes: e.g.
`(= js/Error js/Error) is true in js and cljs but was false in Electric.

src/hyperfiddle/electric/impl/compiler.clj
@@ -237,19 +237,9 @@
        (merge {:file file} debug-info)
        (or debug-info {})))))
 
-(defn bound-js-fn
-  "Given a js global (e.g js/alert or js/console.log), ensures that it is called
-  under the correct `this` context, if it resolves to a function."
-  [sym]
-  (let [fields (clojure.string/split (name sym) #"\.")]
-    `(r/bound-js-fn ~(symbol "js" (if (seq (rest fields))
-                                    (clojure.string/join (interpose '. (butlast fields)))
-                                    "globalThis"))
-       ~sym)))
-
 (defn analyze-global [env sym]
   (case (namespace sym)
-    "js" (ir/eval (bound-js-fn sym))
+    "js" (ir/eval sym)
     (let [sym (if-some [var (env/resolve-var env sym)]
                 (env/var-name var)
                 (case (env/peer-language env)
@@ -329,10 +319,35 @@
         (pure-res (analyze-global env (env/resolve-alias env sym))) ; pass through
         ))))
 
+(defn js-var? [env sym]
+  (when-let [var (env/resolve-var env sym)]
+    (when (instance? CljsVar var)
+      (= 'js (:ns (env/get-var var))))))
+
+(defn bound-js-fn
+  "Given a js global resolving to a function (e.g js/alert, js/console.log required-js-ns/js-fn), ensures it
+  is called under the correct `this` context."
+  [sym]
+  (let [fields (clojure.string/split (name sym) #"\.")]
+    `(.bind ~sym ~(symbol (namespace sym)
+                    (if (seq (rest fields))
+                      (clojure.string/join (interpose '. (butlast fields)))
+                      "globalThis")))))
+
 (defn analyze-apply [env sexp]
-  (->> sexp
-    (map (partial analyze-form env))
-    (apply map-res ir/apply)))
+  (let [[f & args] sexp] ; f may be composite
+    (apply map-res ir/apply
+      (cons (if (and (symbol? f) (js-var? env f))
+              ;; Special case - we must preserve `this` scope when calling a js fn from its global var (e.g.
+              ;; js/alert). Can’t move this logic to `analyze-global` without damaging equality semantics: a
+              ;; bound js fn is not equal to the original fn, so (= js/alert js/alert) would return true in
+              ;; cljs and false in Electric. As of 2023 there is no reliable way to detect a js Class from a
+              ;; Function, moving this logic to `analyze-global` would bind all classes. While it seems
+              ;; harmless, we shouldn't alter userland values unless strictly necessary.
+              ;; See `js_calls_test.cljs`.
+              (pure-res (ir/eval (bound-js-fn f)))
+              (analyze-form env f))
+        (map (partial analyze-form env) args)))))
 
 (defn causal [stmt expr]
   (ir/apply (ir/literal {}) stmt expr))

src/hyperfiddle/electric/impl/runtime.cljc
@@ -105,12 +105,6 @@
   (partial m/latest
     (fn [x y] (if (instance? Failure y) y x))))
 
-#?(:cljs
-   (defn bound-js-fn [this ^js obj]
-     (if (instance? js/Function obj)
-       (.bind obj this)
-       obj)))
-
 (defn pure [x] (m/cp x))
 
 (def empty-event

test/hyperfiddle/js_calls_test.cljs
@@ -1,32 +1,42 @@
 (ns hyperfiddle.js-calls-test
   (:require [hyperfiddle.electric :as e]
             [hyperfiddle.rcf :as rcf :refer [tests tap % with]]
-            ["./js_calls_test" :as call-test]
-            [hyperfiddle.electric.impl.runtime :as r]))
+            ["./js_calls_test" :as call-test]))
+
+;;; Goal: confirm Electric and CLJS have the same js function call semantics.
 
 (call-test/install) ; required for later tests
 
+;;; The two tests blocks should be identical in intent and result.
+
+;; CLJS
 (tests
   "js scoped call in cljs"
+  call-test/scope.fn := call-test/scope.fn
   (call-test/scope.fn) := "value"
   (.fn call-test/scope) := "value"
   (js/hyperfiddle.js_calls_test.scope.fn) := "value" ; requires `(call-test/install)`
   (let [fn (.-fn call-test/scope)]
-    (undefined? (fn)) := true                       ; fn lost its `this` context
-    ((r/bound-js-fn call-test/scope fn)) := "value" ; re-set `this` context to `scope`
+    (undefined? (fn)) := true               ; fn lost its `this` context
+    ((.bind fn call-test/scope)) := "value" ; re-set `this` context to `scope`
     ))
 
+;; Electric
 (tests
   "js scoped call in electric"
   (with (e/run
-          (tap (call-test/scope.fn)) ; direct access
-          (tap (.fn call-test/scope)) ; two-step access
+          (tap call-test/scope.fn)
+          (tap (call-test/scope.fn))    ; direct access
+          (tap (.fn call-test/scope))   ; two-step access
           (tap (js/hyperfiddle.js_calls_test.scope.fn)) ; global access, requires `(call-test/install)`
           (let [fn (.-fn call-test/scope)]
-            (tap (undefined? (fn)))))
+            (tap (undefined? (fn)))
+            (tap ((.bind fn call-test/scope)))))
+    % := call-test/scope.fn
     % := "value"
     % := "value"
     % := "value"
     % := true
+    % := "value"
     ))
 