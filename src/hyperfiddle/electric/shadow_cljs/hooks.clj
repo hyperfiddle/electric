a6f02835924afc5fedf19eed1c1de518db1a6daf
xificurC <xificurC@gmail.com>
2023-11-30 15:39:21 +0100

    check for :dev mode in shadow hook
    thanks for the suggestion @eneroth

--- src/hyperfiddle/electric/shadow_cljs/hooks.clj
+++ src/hyperfiddle/electric/shadow_cljs/hooks.clj
@@ -9,11 +9,12 @@
 
   The reload is necessary to keep the server and client portion of an e/def in sync."
   {:shadow.build/stage :compile-finish} [build-state]
-  (let [compiled-keys (-> build-state :shadow.build/build-info :compiled)
-        cljc-infos (eduction (filter (fn [[_ f]] (str/ends-with? f ".cljc")))
-                     (map #(get (:sources build-state) %)) compiled-keys)]
-    (doseq [{ns-sym :ns, macro-requires :macro-requires} cljc-infos]
-      (when (and (not (get macro-requires ns-sym)) (-> ns-sym find-ns meta ::lang/has-edef?) (needs-reload? ns-sym))
-        (prn ::reloading ns-sym)
-        (require ns-sym :reload))))
+  (when (= :dev (:shadow.build/mode build-state))
+    (let [compiled-keys (-> build-state :shadow.build/build-info :compiled)
+          cljc-infos (eduction (filter (fn [[_ f]] (str/ends-with? f ".cljc")))
+                       (map #(get (:sources build-state) %)) compiled-keys)]
+      (doseq [{ns-sym :ns, macro-requires :macro-requires} cljc-infos]
+        (when (and (not (get macro-requires ns-sym)) (-> ns-sym find-ns meta ::lang/has-edef?) (needs-reload? ns-sym))
+          (prn ::reloading ns-sym)
+          (require ns-sym :reload)))))
   build-state)
