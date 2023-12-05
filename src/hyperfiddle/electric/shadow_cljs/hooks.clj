fdab86e3e984621b7efc2b0b8b218f65efeeaad5
xificurC <xificurC@gmail.com>
2023-12-05 12:35:11 +0100

    shadow dev reload hook: correctly skip first run
    The previous strategy was flawed, we skipped the first reload of every
    file. A file loaded from cache will not recompile on first run.
    
    The new strategy is correct, we don't run the hook on first run.

--- src/hyperfiddle/electric/shadow_cljs/hooks.clj
+++ src/hyperfiddle/electric/shadow_cljs/hooks.clj
@@ -3,18 +3,19 @@
             [hyperfiddle.electric.impl.lang :as lang]
             [clojure.string :as str]))
 
-(let [! (volatile! #{})] (defn needs-reload? [nssym] (or (@! nssym) (do (vswap! ! conj nssym) false))))
-
-(defn reload-clj "If a cljc file with electric defs changed and doesn't require-macros itself, reload it.
+(let [!first-run? (volatile! true)]     ; first run is noop
+  (defn reload-clj "If a cljc file with electric defs changed and doesn't require-macros itself, reload it.
 
   The reload is necessary to keep the server and client portion of an e/def in sync."
-  {:shadow.build/stage :compile-finish} [build-state]
-  (when (= :dev (:shadow.build/mode build-state))
-    (let [compiled-keys (-> build-state :shadow.build/build-info :compiled)
-          cljc-infos (eduction (filter (fn [[_ f]] (str/ends-with? f ".cljc")))
-                       (map #(get (:sources build-state) %)) compiled-keys)]
-      (doseq [{ns-sym :ns, macro-requires :macro-requires} cljc-infos]
-        (when (and (not (get macro-requires ns-sym)) (-> ns-sym find-ns meta ::lang/has-edef?) (needs-reload? ns-sym))
-          (prn ::reloading ns-sym)
-          (require ns-sym :reload)))))
-  build-state)
+    {:shadow.build/stage :compile-finish} [build-state]
+    (if @!first-run?
+      (vreset! !first-run? false)
+      (when (= :dev (:shadow.build/mode build-state))
+        (let [compiled-keys (-> build-state :shadow.build/build-info :compiled)
+              cljc-infos (eduction (filter (fn [[_ f]] (str/ends-with? f ".cljc")))
+                           (map #(get (:sources build-state) %)) compiled-keys)]
+          (doseq [{ns-sym :ns, macro-requires :macro-requires} cljc-infos]
+            (when (and (not (get macro-requires ns-sym)) (-> ns-sym find-ns meta ::lang/has-edef?))
+              (prn ::reloading ns-sym)
+              (require ns-sym :reload))))))
+    build-state))
