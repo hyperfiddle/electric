f97308be175820bca5e780eb6224c352a88b839b
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2023-12-08 11:32:37 +0100

    Fix allow-large-shadow-cache hook
    Would break build without Jackson on the classpath
    Hook is now a noop if Jackson is not on the classpath

--- src/hyperfiddle/electric/shadow_cljs/hooks.clj
+++ src/hyperfiddle/electric/shadow_cljs/hooks.clj
@@ -20,14 +20,17 @@
               (require ns-sym :reload))))))
     build-state))
 
-(defn jackson-version-available? []
-  (try
-    (Class/forName "com.fasterxml.jackson.databind.cfg.PackageVersion")
-    true
-    (catch ClassNotFoundException _
-      false)))
+(defmacro when-class-available
+  "Expands to `body` in a java class named by `class-sym` is available on the classpath at compile time.
+   Expands to nil otherwise."
+  [class-sym & body]
+  (when (try (Class/forName (name class-sym))
+             (catch ClassNotFoundException _ false))
+    `(do ~@body)))
 
-(defn jackson-version [] (bean com.fasterxml.jackson.databind.cfg.PackageVersion/VERSION))
+(defn jackson-version []
+  (when-class-available com.fasterxml.jackson.core.json.PackageVersion
+    (bean com.fasterxml.jackson.core.json.PackageVersion/VERSION)))
 
 (defn is-jackson-limiting-string-size?
   "Read Jackson’s version and return:
@@ -35,14 +38,14 @@
   - ::limited if version is 2.15 or 2.15.1,
   - ::configurable if version is at least 2.15.2,
   - ::unavailable if Jackson is not on the classpath." []
-  (if-not (jackson-version-available?)
-    ::unavailable
-    (let [{:keys [majorVersion minorVersion patchLevel]} (jackson-version)]
+  (if-let [version (jackson-version)]
+    (let [{:keys [majorVersion minorVersion patchLevel]} version]
       (cond
         (and (<= majorVersion 2) (< minorVersion 15))                   ::no-limit     ; < 2.15, no string size limit, we are good.
         (and (<= majorVersion 2) (<= minorVersion 15) (< 2 patchLevel)) ::limited      ; < 2.15.2, with hard-coded limits (non-configurable)
         :else                                                           ::configurable ; > 2.15.2, limits can be configured, we are good.
-        ))))
+        ))
+    ::unavailable))
 
 
 (let [!jackson-string-size-limit-setting (atom nil)]
@@ -91,34 +94,35 @@
      (allow-large-shadow-cache build-state 1))
     ([build-state factor]
      (assert (nat-int? factor) "Cache size factor should be a positive integer.")
-     (case (:shadow.build/stage build-state)
-       :compile-prepare (case (is-jackson-limiting-string-size?)
-                          ::unavailable nil
-                          ::no-limit    nil
-                          ::limited     (println "Your Shadow-cljs setup is too old or one of you dependencies pulled in a version of Jackson older than 2.15.2 ."
-                                          "Electric Clojure hot code reload and recompilation might be slow due to Shadow failing to read large cache files."
-                                          "Update your dependencies to get Jackson >= 2.15.2 for Electric Clojure to benefit from Shadow-cljs caching."
-                                          "See `hyperfiddle.electric.shadow-cljs.hooks/allow-large-shadow-cache`") ; TODO permalink to github source
-                          ::configurable
-                          (swap! !jackson-string-size-limit-setting
-                            (fn [current-default-setting]
-                              (if (some? current-default-setting)
-                                (do (println "Electric Clojure detected two concurrent Shadow-cljs compilations. Client and Server programs might be misaligned.")
-                                    current-default-setting) ; another compilation is in progress, don’t race with it.
-                                (let [current-defaults                                          (com.fasterxml.jackson.core.StreamReadConstraints/defaults)
-                                      {:keys [maxNestingDepth maxNumberLength maxStringLength]} (bean current-defaults)]
-                                  #_(println "Snapshoting default jackson limits" (dissoc (bean current-defaults) :class))
-                                  (com.fasterxml.jackson.core.StreamReadConstraints/overrideDefaultStreamReadConstraints
-                                    (.build (doto (com.fasterxml.jackson.core.StreamReadConstraints/builder)
-                                              (.maxStringLength (* factor maxStringLength))
-                                              (.maxNumberLength maxNumberLength)
-                                              (.maxNestingDepth maxNestingDepth))))
-                                  current-defaults)))))
-       :flush           (swap! !jackson-string-size-limit-setting
-                (fn [current-default-setting]
-                  (when current-default-setting
-                    #_(println "restoring default jackson settings " (dissoc (bean current-default-setting) :class))
-                    (com.fasterxml.jackson.core.StreamReadConstraints/overrideDefaultStreamReadConstraints current-default-setting)
-                    nil)))
-       nil)
+     (when-class-available com.fasterxml.jackson.core.json.PackageVersion
+         (case (:shadow.build/stage build-state)
+           :compile-prepare (case (is-jackson-limiting-string-size?)
+                              ::unavailable nil
+                              ::no-limit    nil
+                              ::limited     (println "Your Shadow-cljs setup is too old or one of you dependencies pulled in a version of Jackson older than 2.15.2 ."
+                                              "Electric Clojure hot code reload and recompilation might be slow due to Shadow failing to read large cache files."
+                                              "Update your dependencies to get Jackson >= 2.15.2 for Electric Clojure to benefit from Shadow-cljs caching."
+                                              "See `hyperfiddle.electric.shadow-cljs.hooks/allow-large-shadow-cache`") ; TODO permalink to github source
+                              ::configurable
+                              (swap! !jackson-string-size-limit-setting
+                                (fn [current-default-setting]
+                                  (if (some? current-default-setting)
+                                    (do (println "Electric Clojure detected two concurrent Shadow-cljs compilations. Client and Server programs might be misaligned.")
+                                        current-default-setting) ; another compilation is in progress, don’t race with it.
+                                    (let [current-defaults                                          (com.fasterxml.jackson.core.StreamReadConstraints/defaults)
+                                          {:keys [maxNestingDepth maxNumberLength maxStringLength]} (bean current-defaults)]
+                                      #_(println "Snapshoting default jackson limits" (dissoc (bean current-defaults) :class))
+                                      (com.fasterxml.jackson.core.StreamReadConstraints/overrideDefaultStreamReadConstraints
+                                        (.build (doto (com.fasterxml.jackson.core.StreamReadConstraints/builder)
+                                                  (.maxStringLength (* factor maxStringLength))
+                                                  (.maxNumberLength maxNumberLength)
+                                                  (.maxNestingDepth maxNestingDepth))))
+                                      current-defaults)))))
+           :flush           (swap! !jackson-string-size-limit-setting
+                              (fn [current-default-setting]
+                                (when current-default-setting
+                                  #_(println "restoring default jackson settings " (dissoc (bean current-default-setting) :class))
+                                  (com.fasterxml.jackson.core.StreamReadConstraints/overrideDefaultStreamReadConstraints current-default-setting)
+                                  nil)))
+           nil))
      build-state)))
