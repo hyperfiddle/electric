a7e2772d09aa80b7d30907ac4d9a1be099c8c722
Léo NOEL <contact@leonoel.fr>
2023-12-19 10:25:45 +0100

    low-level API

--- test/hyperfiddle/electric_compiler_test.clj
+++ test/hyperfiddle/electric_compiler_test.clj
@@ -23,128 +23,172 @@
   (lang/compile-client {} 1)
   ;; r/defs takes & flows with an implicit context (managed in runtime, thread-local or such)
   ;; context - path of the node you're constructing in the call stack
-  := `(r/defs (r/static 1))
+  := `(r/peer
+        (lang/defs
+          (lang/static 1))
+        [] 0)
 
   ;; transfer
   (lang/compile-client {}
     `(e/client "Hello world")) :=
-  `(r/defs
-     (r/static "Hello world"))
+  `(r/peer
+     (lang/defs
+       (lang/static "Hello world"))
+     [] 0)
   (lang/compile-client {}
     `(e/client
        (let [x "Hello world"
              y "Hello world"]
          [x y]))) :=
-  `(r/defs
-     (r/ap (r/static vector) (r/local 1) (r/local 2))
-     (r/static "Hello world")
-     (r/static "Hello world"))
+  `(r/peer
+     (lang/defs
+       (lang/static "Hello world")
+       (lang/static "Hello world")
+       (lang/ap (lang/static vector) (lang/local 1) (lang/local 2)))
+     [] 2)
 
   (lang/compile-server {}
     `(e/client "Hello world")) :=
-  `(r/defs
-     (r/remote 0))                                           ;; 0 refers to client's r/defs
+  `(r/peer
+     (lang/defs
+       (lang/remote 0))  ;; 0 refers to client's r/defs
+     [] 0)
 
   ;; function application
   (lang/compile-client {}
     `(e/client (prn "Hello world"))) :=
-  `(r/defs
-     (r/ap (r/static prn) (r/static "Hello world")))
+  `(r/peer
+     (lang/defs
+       (lang/ap (lang/static prn) (lang/static "Hello world")))
+     [] 0)
 
   ;; lexical scope
   (lang/compile-client {}
     `(e/client (let [a :foo] [a a]))) :=
-  `(r/defs
-     (r/ap (r/static vector) (r/local 1) (r/local 1))
-     (r/static :foo))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/ap (lang/static vector) (lang/local 1) (lang/local 1)))
+     [] 1)
 
   (lang/compile-client {}
     `(e/client
        (let [a (e/server :foo)]
          (e/server (prn a))))) :=
-  `(r/defs
-     (r/remote 0))
+  `(r/peer
+     (lang/defs
+       (lang/remote 0))
+     [] 0)
   (lang/compile-server {}
     `(e/client
        (let [a (e/server :foo)]
          (e/server (prn a))))) :=
-  `(r/defs
-     (r/ap (r/static prn) (r/local 1))
-     (r/static :foo))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/ap (lang/static prn) (lang/local 0)))
+     [] 1)
 
   ;; join (e/watch !x)
   ;; (i/fixed continuous-flow) -> incremental sequence of 1 element
   (lang/compile-client {}
     `(e/client (e/join (i/fixed (m/watch !x))))) :=
-  `(r/defs
-     (r/join (r/ap (r/static i/fixed) (r/ap (r/static m/watch) (r/static !x)))))
+  `(r/peer
+     (lang/defs
+       (lang/join (lang/ap (lang/static i/fixed)
+                    (lang/ap (lang/static m/watch)
+                      (lang/static !x)))))
+     [] 0)
 
   ;; pure (get the incseq of an expression) (e/pure (e/join x)) is (e/join (e/pure x)) is x
   (lang/compile-client {}
     `(e/client (e/pure :foo))) :=
-  `(r/defs
-     (r/pure (r/static :foo)))
+  `(r/peer
+     (lang/defs
+       (lang/static (lang/static :foo)))
+     [] 0)
 
   ;; ctor (e/fn [] foo) -> (e/ctor foo) (previously ::c/closure)
   (lang/compile-client {}
     `(e/client (e/ctor :foo))) :=
-  `(r/defs
-     (r/ctor 1)
-     (r/static :foo))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/ctor [] 0))
+     [] 1)
 
   ;; call (aka new, but with no argument and only for ctors)
   (lang/compile-client {}
     `(e/client (e/call (e/ctor :foo)))) :=
-  `(r/defs
-     (r/call (r/ctor 1))
-     (r/static :foo))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/ctor [] 0)
+       (lang/call 0))
+     [1] 2)
 
   ;; lexical closure
   (lang/compile-client {}
     `(e/client
        (let [a :foo]
          (e/call (e/ctor a))))) :=
-  `(r/defs
-     (r/call (r/ctor 1 (r/local 2)))
-     (r/free 0)
-     (r/static :foo))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/free 0)
+       (lang/ctor [] 1 (lang/local 0))
+       (lang/call 0))
+     [2] 3)
 
   (lang/compile-client {}
     `(e/client
        (let [a :foo]
          (e/call (e/ctor (e/ctor a)))))) :=
-  `(r/defs
-     (r/call (r/ctor 1 (r/local 3)))
-     (r/ctor 2 (r/free 0))
-     (r/free 0)
-     (r/static :foo))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/free 0)
+       (lang/ctor [] 1 (lang/free 0))
+       (lang/ctor [] 2 (lang/local 0))
+       (lang/call 0))
+     [3] 4)
 
   ;; conditionals
   (lang/compile-client {}
     `(e/client (case :x nil :y :z))) :=
-  `(r/defs
-     (r/call (r/ap (r/ap (r/static hash-map)
-                     (r/static nil) (r/ctor 1))
-               (r/static :x) (r/ctor 2)))
-     (r/static :y)
-     (r/static :z))
+  `(r/peer
+     (lang/defs
+       (lang/static :y)
+       (lang/static :z)
+       (lang/ap (lang/ap (lang/static hash-map)
+                  (lang/static nil) (lang/ctor [] 0))
+         (lang/static :x) (lang/ctor [] 1))
+       (lang/call 0))
+     [2] 3)
 
   ;; var
   (e/def x)
   (lang/compile-client {} `(e/client (var x))) :=
-  `(r/defs (r/var (quote x)))
+  `(r/peer
+     (lang/defs
+       (lang/var x))
+     [] 0)
 
   (lang/compile-client {}
     `(let [a :foo, b :bar, c :baz]
        [(e/ctor [a b]) (e/ctor [b c])])) :=
-  `(r/defs
-     (r/ap (r/static vector)
-       (r/ctor 4 (r/local 1) (r/local 2))
-       (r/ctor 5 (r/local 2) (r/local 3)))
-     (r/static :foo)
-     (r/static :bar)
-     (r/static :baz)
-     (r/ap (r/static vector) (r/free 0) (r/free 1))
-     (r/ap (r/static vector) (r/free 0) (r/free 1)))
+  `(r/peer
+     (lang/defs
+       (lang/static :foo)
+       (lang/static :bar)
+       (lang/static :baz)
+       (lang/ap (lang/static vector)
+         (lang/free 0) (lang/free 1))
+       (lang/ap (lang/static vector)
+         (lang/free 0) (lang/free 1))
+       (lang/ap (lang/static vector)
+         (lang/ctor 3 (lang/local 0) (lang/local 1))
+         (lang/ctor 4 (lang/local 1) (lang/local 2))))
+     [] 5)
 
   )
