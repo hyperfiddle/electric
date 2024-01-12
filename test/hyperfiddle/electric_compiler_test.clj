63920e5eb441ecea17370ec22c55d662f62efdbd
Léo NOEL <contact@leonoel.fr>
2024-01-12 10:52:43 +0100

    new design with dynamic sites + unified defs

--- test/hyperfiddle/electric_compiler_test.clj
+++ test/hyperfiddle/electric_compiler_test.clj
@@ -22,6 +22,7 @@
     (try (apply f args)
          (catch Throwable e (find-source-map-info path))))
 
+;; TODO rewrite or remove
 (tests
   (l/compile-client 1) :=  `(r/peer (lang/r-defs (lang/r-static 1)) [] 0)
   (l/compile-server 1) :=  `(r/peer (lang/r-defs (lang/r-static 1)) [] 0)
@@ -316,223 +317,219 @@
   ;; (let [sm (l/compile-client-with-source-map (set! (.-x (Object.)) 1))])
   )
 
+;; cdef = definition of the static structure of an e/ctor
+;; args :
+;; * free variable count
+;; * a vector of node sites
+;; * a vector of call sites
+;; * the result site
 (comment
-  ;; (defn lang/compile [env form]
-  (l/compile-client 1)
-  ;; r/defs takes & flows with an implicit context (managed in runtime, thread-local or such)
-  ;; context - path of the node you're constructing in the call stack
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static 1))
-        [] 0)
+  (l/compile ::Main 1)
+  := `[(r/cdef 0 [] [] nil
+         (fn [frame]
+           (r/pure 1)))]
 
-  ;; transfer
-  (l/compile-client (e/client "Hello world")) :=
-  `(r/peer
-     (lang/r-defs
-       (lang/r-static "Hello world"))
-     [] 0)
-  (l/compile-client (e/client
-                      (let [x "Hello world"
-                            y "Hello world"]
-                        [x y])))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static "Hello world")
-          (lang/r-static "Hello world")
-          (lang/r-ap (lang/r-static vector) (lang/r-local 0) (lang/r-local 1)))
-        [] 2)
-
-  (l/compile-server (e/client "Hello world"))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-remote 0)) ;; 0 refers to client's r/defs
-        [] 0)
+  (l/compile ::Main `(e/client "Hello world")) :=
+  `[(r/cdef 0 [] [] :client
+      (fn [frame]
+        (r/pure "Hello world")))]
 
   ;; function application
-  (l/compile-client (e/client (prn "Hello world")))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-ap (lang/r-static prn) (lang/r-static "Hello world")))
-        [] 0)
+  (l/compile ::Main (e/client (prn "Hello world")))
+  := `[(r/cdef 0 [] [] :client
+         (fn [frame]
+           (r/ap (r/lookup frame :clojure.core/prn (r/pure prn)) (r/pure "hello world"))))]
 
   ;; lexical scope
-  (l/compile-client (e/client (let [a :foo] [a a])))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-ap (lang/r-static vector) (lang/r-local 1) (lang/r-local 1)))
-        [] 1)
+  (l/compile ::Main (e/client (let [a :foo] [a a])))
+  := `[(r/cdef 0 [:client] [] :client
+         (fn [frame]
+           (r/define-node frame 0 (r/pure :foo))
+           (r/ap (r/lookup frame :clojure.core/vector (r/pure vector))
+             (r/node frame 0) (r/node frame 0))))]
 
-  (l/compile-client (e/client
-                      (let [a (e/server :foo)]
-                        (e/server (prn a)))))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-remote 0))
-        [] 0)
-  (l/compile-server (e/client
-                      (let [a (e/server :foo)]
-                        (e/server (prn a)))))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-ap (lang/r-static prn) (lang/r-local 0)))
-        [] 1)
+  ;; lexical scope
+  (l/compile ::Main `(e/client
+                       (let [x "Hello"
+                             y "world"]
+                         [x y])))
+  := `[(r/cdef 0 [:client :client] [] :client
+         (fn [frame]
+           (r/define-node frame 0 (r/pure "Hello"))
+           (r/define-node frame 1 (r/pure "world"))
+           (r/ap (r/lookup frame :clojure.core/vector (r/pure vector))
+             (r/node frame 0) (r/node frame 1))))]
 
+  (l/compile ::Main `(e/client
+                       (let [a (e/server :foo)]
+                         (e/server (prn a)))))
+  := `[(r/cdef 0 [:server] [] :server
+         (fn [frame]
+           (r/define-node frame 0 (r/pure :foo))
+           (r/ap (r/lookup frame :clojure.core/prn (r/pure prn))
+             (r/node frame 0))))]
+
+  ;; (def !x (atom 0))
   ;; join (e/watch !x)
   ;; (i/fixed continuous-flow) -> incremental sequence of 1 element
-  (l/compile-client `(e/client (e/join (i/fixed (m/watch !x)))))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-join (lang/r-ap (lang/r-static i/fixed)
-                         (lang/r-ap (lang/r-static m/watch)
-                           (lang/r-static !x)))))
-        [] 0)
+  (l/compile ::Main `(e/client (e/join (i/fixed (m/watch !x)))))
+  := `[(r/cdef 0 [] [] :client
+         (fn [frame]
+           (r/join
+             (r/ap (r/lookup frame ::i/fixed (r/pure i/fixed))
+               (r/ap (r/lookup frame ::m/watch (r/pure m/watch))
+                 (r/lookup frame ::!x (r/pure !x)))))))]
 
   ;; pure (get the incseq of an expression) (e/pure (e/join x)) is (e/join (e/pure x)) is x
-  (l/compile-client (e/client (e/pure :foo)))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static (lang/r-static :foo)))
-        [] 0)
+  (l/compile ::Main `(e/client (e/pure :foo)))
+  := `[(r/cdef 0 [] [] :client
+         (fn [frame]
+           (r/pure (r/pure :foo))))]
 
   ;; ctor (e/fn [] foo) -> (e/ctor foo) (previously ::c/closure)
-  (l/compile-client (e/client (e/ctor :foo)))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-ctor [] 0))
-        [] 1)
+  (l/compile ::Main `(e/client (e/ctor :foo)))
+  := `[(r/cdef 0 [] [] :client
+         (fn [frame]
+           (r/pure (r/make-ctor frame ::Main 1))))
+       (r/cdef 0 [] [] nil
+         (fn [frame]
+           (r/pure :foo)))]
 
   ;; call (aka new, but with no argument and only for ctors)
-  (l/compile-client (e/client (e/call (e/ctor :foo))))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-ctor [] 0)
-          (lang/r-call 0))
-        [1] 2)
+  (l/compile ::Main `(e/client (e/call (e/ctor :foo))))
+  := `[(r/cdef 0 [] [:client] :client
+         (fn [frame]
+           (r/define-call frame 0 (r/pure (r/make-ctor frame ::Main 1)))
+           (r/join (r/call frame 0))))
+       (r/cdef 0 [] [] nil
+         (fn [frame]
+           (r/pure :foo)))]
 
   ;; lexical closure
-  (l/compile-client (e/client
-                      (let [a :foo]
-                        (e/call (e/ctor a)))))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-free 0)
-          (lang/r-ctor [] 1 (lang/r-local 0))
-          (lang/r-call 0))
-        [2] 3)
+  (l/compile ::Main `(e/client
+                       (let [a :foo]
+                         (e/call (e/ctor a)))))
+  := `[(r/cdef 0 [:client] [:client] :client
+         (fn [frame]
+           (r/define-node frame 0 (r/pure :foo))
+           (r/define-call frame 0 (r/pure (doto (r/make-ctor frame ::Main 1)
+                                            (r/define-free 0 (r/node frame 0)))))
+           (r/join (r/call frame 0))))
+       (r/cdef 1 [] [] nil
+         (fn [frame]
+           (r/free frame 0)))]
 
-  (l/compile-client (e/client
-                      (let [a :foo]
-                        (e/call (e/ctor (e/ctor a))))))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-free 0)
-          (lang/r-ctor [] 1 (lang/r-free 0))
-          (lang/r-ctor [] 2 (lang/r-local 0))
-          (lang/r-call 0))
-        [3] 4)
+  (l/compile ::Main `(e/client
+                       (let [a :foo]
+                         (e/call (e/ctor (e/ctor a))))))
+  := `[(r/cdef 0 [:client] [:client] :client
+         (fn [frame]
+           (r/define-node frame 0 (r/pure :foo))
+           (r/define-call frame 0 (r/pure (doto (r/make-ctor frame ::Main 1)
+                                            (r/define-free 0 (r/node frame 0)))))
+           (r/join (r/call frame 0))))
+       (r/cdef 1 [] [] nil
+         (fn [frame]
+           (r/pure (doto (r/make-ctor frame ::Main 2)
+                     (r/define-free 0 (r/free frame 0))))))
+       (r/cdef 1 [] [] nil
+         (fn [frame]
+           (r/free frame 0)))]
 
   ;; conditionals
-  (l/compile-client `(e/client (case :x nil :y :z)))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :y)
-          (lang/r-static :z)
-          (lang/r-ap (lang/r-ap (lang/r-static hash-map)
-                       (lang/r-static nil) (lang/r-ctor [] 0))
-            (lang/r-static :x) (lang/r-ctor [] 1))
-          (lang/r-call 0))
-        [2] 3)
+  (l/compile ::Main `(case :x nil :y :z))
+  := `[(r/cdef 0 [] [nil] nil
+         (fn [frame]
+           (r/define-call frame 0
+             (r/ap
+               (r/ap
+                 (r/lookup frame :clojure.core/hash-map (r/pure hash-map))
+                 (r/pure nil) (r/make-ctor frame ::Main 1))
+               (r/pure :x)
+               (r/pure (r/make-ctor frame ::Main 2))))
+           (r/join (r/call frame 0))))
+       (r/cdef 0 [] [] nil
+         (fn [frame]
+           (r/pure :y)))
+       (r/cdef 0 [] [] nil
+         (fn [frame]
+           (r/pure :z)))]
 
-  ;; var
-  (e/def x)
-  (l/compile-client (e/client (var x)))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-var x))
-        [] 0)
+  (l/compile ::Main (new (e/fn Foo [] (Foo.))))
+  := `[(r/cdef 0 [] [nil] nil
+         (fn [frame]
+           (r/define-call frame 0
+             (let [ctor (r/make-ctor frame ::Main 1)]
+               (r/define-free ctor 0 (r/pure ctor))
+               (r/pure ctor)))
+           (r/join (r/call frame 0))))
+       (r/cdef 1 [] [nil] nil
+         (fn [frame]
+           (r/define-call frame 0 (r/free frame 0))
+           (r/join (r/call frame 0))))]
 
-  (l/compile-client (let [a :foo, b :bar, c :baz]
-                      [(e/ctor [a b]) (e/ctor [b c])]))
-  := `(r/peer
-        (lang/r-defs
-          (lang/r-static :foo)
-          (lang/r-static :bar)
-          (lang/r-static :baz)
-          (lang/r-ap (lang/r-static vector)
-            (lang/r-free 0) (lang/r-free 1))
-          (lang/r-ap (lang/r-static vector)
-            (lang/r-free 0) (lang/r-free 1))
-          (lang/r-ap (lang/r-static vector)
-            (lang/r-ctor 3 (lang/r-local 0) (lang/r-local 1))
-            (lang/r-ctor 4 (lang/r-local 1) (lang/r-local 2))))
-        [] 5)
-
-  (l/compile-client (new (e/fn Foo [] (Foo.))))
-  := `(r/peer
-        (fn [tier id]
-          (case id
-            0 (r/ctor-free (r/tier-ctor tier) 0)
-            1 (let [free (object-array 1)
-                    ctor (r/peer-ctor (r/tier-peer tier) [] 0 free)]
-                (aset free 0 (r/pure ctor))
-                (r/pure ctor))
-            2 (i/latest-concat (r/tier-slot tier 0))))
-        [1] 2)
-
-  (l/compile-client (e/letfn [(Foo [] (Bar.))
+  (l/compile ::Main (e/letfn [(Foo [] (Bar.))
                               (Bar [] (Foo.))]
                       (Foo.)))
-  := `(r/peer
-        (fn [tier id]
-          (case id
-            0 (r/ctor-free (r/tier-ctor tier) 1)
-            1 (r/ctor-free (r/tier-ctor tier) 0)
-            2 (let [Foo-free (object-array 2)
-                    Foo-ctor (r/peer-ctor (r/tier-peer tier) [] 0 Foo-free)
-                    Bar-free (object-array 2)
-                    Bar-ctor (r/peer-ctor (r/tier-peer tier) [] 1 Bar-free)]
-                (aset Foo-free 0 (r/pure Foo-ctor))
-                (aset Foo-free 1 (r/pure Bar-ctor))
-                (aset Bar-free 0 (r/pure Foo-ctor))
-                (aset Bar-free 1 (r/pure Bar-ctor))
-                (r/pure {:Foo Foo-ctor :Bar Bar-ctor}))
-            3 (i/latest-product :Foo (r/tier-local tier 0))
-            4 (i/latest-concat (r/tier-slot tier 0))))
-        [3] 4)
+  := `[(r/cdef 0 [] [nil] nil
+         (fn [frame]
+           (let [ctor-foo (r/make-ctor frame ::Main 1)
+                 ctor-bar (r/make-ctor frame ::Main 2)]
+             (r/define-free ctor-foo 0 (r/pure ctor-foo))
+             (r/define-free ctor-foo 1 (r/pure ctor-bar))
+             (r/define-free ctor-bar 0 (r/pure ctor-bar))
+             (r/define-free ctor-bar 1 (r/pure ctor-foo))
+             (r/define-call frame 0 (r/pure ctor-foo))
+             (r/join (r/call frame 0)))))
+       (r/cdef 2 [] [nil] nil
+         (fn [frame]
+           (r/define-call frame 0 (r/free frame 1))
+           (r/join (r/call frame 0))))
+       (r/cdef 2 [] [nil] nil
+         (fn [frame]
+           (r/define-call frame 0 (r/free frame 1))
+           (r/join (r/call frame 0))))]
 
-  (l/compile-client (let [a :foo, b :bar, c :baz]
+  (l/compile ::Main (let [a :foo, b :bar, c :baz]
                       [(e/ctor [a b]) (e/ctor [b c])]))
-  := `(lang/r-defs
-        (lang/r-ap (lang/r-static vector)
-          (lang/r-ctor 4 (lang/r-local 1) (lang/r-local 2))
-          (lang/r-ctor 5 (lang/r-local 2) (lang/r-local 3)))
-        (lang/r-static :foo)
-        (lang/r-static :bar)
-        (lang/r-static :baz)
-        (lang/r-ap (lang/r-static vector) (lang/r-free 0) (lang/r-free 1))
-        (lang/r-ap (lang/r-static vector) (lang/r-free 0) (lang/r-free 1)))
+  := `[(r/cdef 0 [nil nil nil] [] nil
+         (fn [frame]
+           (r/define-node frame 0 (r/pure :foo))
+           (r/define-node frame 1 (r/pure :bar))
+           (r/define-node frame 2 (r/pure :baz))
+           (r/ap (r/lookup frame :clojure.core/vector (r/pure clojure.core/vector))
+             (let [ctor (r/make-ctor frame ::Main 1)]
+               (r/define-free ctor 0 (r/node frame 0))
+               (r/define-free ctor 1 (r/node frame 1))
+               (r/pure ctor))
+             (let [ctor (r/make-ctor frame ::Main 2)]
+               (r/define-free ctor 0 (r/node frame 1))
+               (r/define-free ctor 1 (r/node frame 2))
+               (r/pure ctor)))))
+       (r/cdef 2 [] [] nil
+         (fn [frame]
+           (r/ap (r/lookup frame :clojure.core/vector (r/pure clojure.core/vector))
+             (r/free frame 0)
+             (r/free frame 1))))
+       (r/cdef 2 [] [] nil
+         (fn [frame]
+           (r/ap (r/lookup frame :clojure.core/vector (r/pure clojure.core/vector))
+             (r/free frame 0)
+             (r/free frame 1))))]
 
   #_(e/defn Foo [])
-  (l/compile-client `(e/ctor (e/call Foo))) :=
-  `(r/peer
-     (lang/r-defs
-       (lang/r-lookup Foo)
-       (lang/r-call 0)
-       (lang/r-ctor [0] 1))
-     [] 2)
+  (l/compile ::Main `(e/ctor (e/call Foo))) :=
+  `[(r/cdef 0 [] [] nil
+      (fn [frame]
+        (r/pure (r/make-ctor frame ::Main 1))))
+    (r/cdef 0 [] [nil] nil
+      (fn [frame]
+        (r/define-call frame 0 (r/lookup frame ::Foo (r/pure (r/make-ctor frame ::Foo 0))))
+        (r/join (r/call frame 0))))]
 
-  (l/compile-client `e/tier) :=
-  `(r/peer
-     (fn [tier id]
-       (case id
-         0 (r/pure tier)))
-     [] 0)
+  (l/compile ::Main `e/frame) :=
+  `[(r/cdef 0 [] [] nil
+      (fn [frame]
+        (r/pure frame)))]
 
   )
