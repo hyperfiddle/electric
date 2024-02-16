8174fcbaf724388d08e9d5b2f82e32a41d8248da
xificurC <xificurC@gmail.com>
2024-02-16 17:35:13 +0100

    cljs analyzer: checkpoint

--- test/hyperfiddle/electric/impl/cljs_analyzer_test.clj
+++ test/hyperfiddle/electric/impl/cljs_analyzer_test.clj
@@ -2,11 +2,19 @@
   (:require [clojure.test :as t]
             [hyperfiddle.electric.impl.cljs-analyzer :as ana]))
 
-(t/deftest all
-  (let [a (ana/analyze-ns 'hyperfiddle.electric.impl.cljs-file-to-analyze)]
-    (t/is (nil? (ana/find-var 'non a)))
-    (t/is (nil? (ana/find-var 'first a)))
-    (t/are [x] (some? (ana/find-var x a))
+(comment
+  (def a (ana/analyze-ns 'hyperfiddle.electric.impl.cljs-file-to-analyze))
+  (-> a ::ana/nses (get 'hyperfiddle.electric.impl.cljs-file-to-analyze) ::ana/refers)
+  (ana/find-var a 'next 'hyperfiddle.electric.impl.cljs-file-to-analyze)
+  )
+
+(t/deftest ns-expansion
+  (let [ns$ 'hyperfiddle.electric.impl.cljs-file-to-analyze
+        a (ana/analyze-ns ns$)]
+    (t/is (nil? (ana/find-var a 'non ns$)))
+    (t/is (nil? (ana/find-var a 'first ns$)))
+    (t/is (= 'cljs.core/next (::ana/name (ana/find-var a 'nxt ns$))))
+    (t/are [x] (some? (ana/find-var a x ns$))
       'foo
       'bar
       'baz
@@ -26,6 +34,23 @@
       'behind-use-renamed
       'behind-use-macro
       'behind-use-macro-renamed
-      'nxt
-      ;; 'refnonmacro                        ; TODO
-      )))
+      'behind-auto-alias
+      'behind-auto-alias-alias
+      'behind-auto-alias-refer
+      'nxt)))
+
+(t/deftest runtime-vars
+  (let [ns$ 'hyperfiddle.electric.impl.cljs-file-to-analyze,
+        a (ana/analyze-ns ns$)]
+    (t/are [x] (nil? (ana/find-var a x ns$))
+      'hyperfiddle.electric.impl.cljs-file-to-analyze.runtime/only-macro
+      'run/only-macro
+      'only-macro
+      'next) ; renamed in :refer-clojure
+    (t/are [x] (some? (ana/find-var a x ns$))
+      'hyperfiddle.electric.impl.cljs-file-to-analyze.runtime/macro-and-runtime
+      'run/macro-and-runtime
+      'macro-and-runtime
+      'hyperfiddle.electric.impl.cljs-file-to-analyze.runtime/only-runtime
+      'run/only-runtime
+      'only-runtime)))
---
+++ test/hyperfiddle/electric/impl/cljs_file_to_analyze/runtime.clj
@@ -0,0 +1,4 @@
+(ns hyperfiddle.electric.impl.cljs-file-to-analyze.runtime)
+
+(defmacro only-macro [])
+(defmacro macro-and-runtime [])
