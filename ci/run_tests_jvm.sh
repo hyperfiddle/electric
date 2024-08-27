efb61bc66afd65c93e3ad3864811eea45287fb5d
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2024-08-27 18:41:23 +0200

    Tests all namespaces unless excluded (jvm)

--- ci/run_tests_jvm.sh
+++ ci/run_tests_jvm.sh
@@ -4,8 +4,7 @@ echo "Running JVM tests"
 clojure -X:test \
     :dirs "[\"src\" \"test\"]"  \
     :patterns                                \
-    "[\"hyperfiddle.*-test\"                 \
-      \"hyperfiddle.electric.impl.*\"        \
+    "[\"hyperfiddle.((?!api|popover|txn|electric-fulcro|electric-httpkit|spool|spec).)*\"                 \
       \"contrib.((?!datomic).)*\"            \
      ]"
 
