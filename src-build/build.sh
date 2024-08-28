e6d45b181a0a3d37e1de8524ce88de0e4430d08e
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2024-08-28 17:46:27 +0200

    Clean build script

--- src-build/build.sh
+++ src-build/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-clojure -T:build clean && rm -rf ./resources-demo/public/js
-#clj -A:dev -X user/release -- distribute sources, lib consumer will build
+clojure -T:build clean
 HYPERFIDDLE_ELECTRIC_BUILD=`git describe --tags --long --always --dirty`
 clojure -T:build jar :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
 clojure -T:build install :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
