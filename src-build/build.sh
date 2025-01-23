cf877645310564df64f257a9411373bb1ce0e5f7
Dustin Getz <dustin.getz@gmail.com>
2025-01-23 17:00:28 -0500

    build.sh

--- src-build/build.sh
+++ src-build/build.sh
@@ -5,3 +5,4 @@ clojure -T:build clean
 HYPERFIDDLE_ELECTRIC_BUILD="v3-alpha-SNAPSHOT"
 clojure -T:build jar :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
 clojure -T:build install :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
+#env $(cat .env | xargs) clojure -T:build deploy :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
