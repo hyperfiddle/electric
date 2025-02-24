0ee524eff00a606df2ed04f03c6ff5035a807899
Dustin Getz <dustin.getz@gmail.com>
2025-02-24 09:54:27 -0500

    extract datomic models to src-private

--- src/contrib/test/datomic_sku_model.clj
+++
@@ -1,3 +0,0 @@
-(ns contrib.test.datomic-sku-model)
-
-; https://github.com/Datomic/day-of-datomic/blob/master/tutorial/filters.clj
--- src/contrib/test/datomic_social_news.clj
+++
@@ -1,3 +0,0 @@
-(ns contrib.test.datomic-social-news)
-
-; https://gist.github.com/stuarthalloway/2948756
