4df5554815c38ccfcee08e0a75c707ebe7618674
Dustin Getz <dustin.getz@gmail.com>
2025-03-08 10:23:41 -0500

    publish: improve build scripts

--- src-build/build.sh
+++ src-build/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -eux -o pipefail
 
+# Todo clojure build output needs to log diagnostics to stderr (like set -x) so that
+# stdout can be used to return a value, such as the _BUILD value, to another script.
+
 clojure -T:build clean
 HYPERFIDDLE_ELECTRIC_BUILD="v3-alpha-SNAPSHOT"
 clojure -T:build jar :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
 clojure -T:build install :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
 #env $(cat .env | xargs) clojure -T:build deploy :version '"'$HYPERFIDDLE_ELECTRIC_BUILD'"'
+#env $(cat .env | xargs) clojure -T:build deploy :version '"'v3-alpha-SNAPSHOT'"'
