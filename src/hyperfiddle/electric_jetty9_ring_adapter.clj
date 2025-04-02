e2480749081473ff58737385fec957f3c819eb78
xificurC <xificurC@gmail.com>
2025-04-02 21:40:29 +0200

    [jetty9] simplified adapter, with working gzip example

--- src/hyperfiddle/electric_jetty9_ring_adapter.clj
+++ src/hyperfiddle/electric_jetty9_ring_adapter.clj
@@ -7,6 +7,8 @@
   (:import [missionary Cancelled]
            [java.nio ByteBuffer]
            [java.util.concurrent.atomic AtomicInteger]
+           [org.eclipse.jetty.server Handler]
+           [org.eclipse.jetty.server.handler ContextHandler HandlerList]
            [org.eclipse.jetty.websocket.api Session WebSocketAdapter]))
 
 (def proxy-ws-handler ws-adapter/proxy-ws-handler)
@@ -114,6 +116,8 @@
                    (log/trace "bytes received" {:length length})
                    ((aget state on-message-slot) (ByteBuffer/wrap bytes offset length)))}))
 
+(defn build-electric-websocket-middleware [boot-fn] (fn [ring-req] (electric-ws-adapter ring-req boot-fn)))
+
 (defn reject-websocket-handler
   "Will accept socket connection upgrade and immediately close the socket on
   connection, with given `code` and `reason`. Use this to cleanly reject a
@@ -123,3 +127,53 @@
   ;; 101 failure cause.
   [code reason]
   {:on-connect (fn [^WebSocketAdapter ws] (.. ws getSession (close code reason)))})
+
+#_(def VERSION (not-empty (System/getProperty "ELECTRIC_USER_VERSION"))) ; see Dockerfile
+#_(defn wrap-reject-stale-client
+    "Intercept websocket UPGRADE request and check if client and server versions matches.
+    An electric client is allowed to connect if its version matches the server's version, or if the server doesn't have a version set (dev mode).
+    Otherwise, the client connection is rejected gracefully."
+    [next-handler]
+    (fn [ring-req]
+      (let [client-version (get-in ring-req [:query-params "ELECTRIC_USER_VERSION"])]
+        (cond
+          (nil? VERSION)             (next-handler ring-req)
+          (= client-version VERSION) (next-handler ring-req)
+          :else (adapter/reject-websocket-handler 1008 "stale client") ; https://www.rfc-editor.org/rfc/rfc6455#section-7.4.1
+          ))))
+
+(defn install-websocket "Use under `:configurator` key of `ring.adapter.jetty/run-jetty` to install an electric websocket handler.
+
+  With no middleware
+
+  ```clj
+  (install-websocket jetty-server \"/\" (build-electric-websocket-middleware boot-fn))
+  ```
+
+  where `boot-fn` is e.g. `(fn [ring-req] (e/boot-server {} electric-starter-app.main/Main (e/server ring-req)))`.
+
+  With middleware
+
+  ```clj
+  (install-websocket jetty-server \"/\"
+    (-> (build-electric-websocket-middleware boot-fn)
+      (ring.middleware.cookies/wrap-cookies)
+      (wrap-reject-stale-client)
+      (ring.middleware.params/wrap-params)))
+  ```
+
+  Passes optional `config` forward to `proxy-ws-handler`.
+"
+  ([jetty-server path ring-middleware] (install-websocket jetty-server path ring-middleware {}))
+  ([jetty-server path ring-middleware
+    {:as config :keys [ws-max-idle-time ws-max-text-message-size] :or {ws-max-idle-time 500000 ws-max-text-message-size 65536}}] ; copied from proxy-ws-handler for documentation
+   (letfn [(create-websocket-handler [context-path handler]
+             (doto (ContextHandler.)
+               (.setContextPath context-path) ; matches e.g. "/"
+               (.setAllowNullPathInfo false) ; FIXME can we remove this? not sure what it does for just "/". It's really up to the user to canonicalize urls. https://javadoc.jetty.org/jetty-9/org/eclipse/jetty/server/handler/ContextHandler.html#setAllowNullPathInfo(boolean)
+               (.setHandler (proxy-ws-handler handler config))))
+           (add-websocket-handler [server path ring-middleware]
+             (let [handlers [(create-websocket-handler path ring-middleware) (.getHandler server)]]
+               (.setHandler server (doto (HandlerList.) (.setHandlers (into-array Handler handlers))))))]
+     (doto jetty-server
+       (add-websocket-handler path ring-middleware)))))
